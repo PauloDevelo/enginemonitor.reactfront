fb97be57a1a1869da930fa9c7c4416399929e462
Torruella, Paul <paul.torruella@suez.com>
2019-03-31 10:17:40 +0800

    Migrate ClockLabel to TS

---
+++ src/components/ClockLabel/ClockLabel.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import { FormattedDate, FormattedTime } from 'react-intl';
+
+const ClockLabel = () => {
+    const[currentDate, setCurrentDate] = useState(new Date());
+
+    useEffect(() => {
+        const intervalID = setInterval(
+            () => setCurrentDate(new Date()),
+            1000
+        );
+        // Specify how to clean up after this effect:
+        return function cleanup() {
+            clearInterval(intervalID);
+        };
+      }, []);
+
+    return (
+        <span>
+            <FormattedDate value={currentDate} />
+            <span> </span>
+            <FormattedTime value={currentDate} hour='numeric' minute='numeric' second='numeric'/>
+        </span>
+    );
+}
+
+export default ClockLabel;
