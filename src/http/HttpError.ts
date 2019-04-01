b69ca3d244bd015fd03cddcce8c485c95354d873
Torruella, Paul <paul.torruella@suez.com>
2019-04-01 09:32:54 +0800

    Migrate httpError to TS

---
+++ src/http/HttpError.ts
@@ -0,0 +1,18 @@
+export default class HttpError extends Error {
+  private readonly date:Date;
+  private readonly data:any;
+
+  constructor(data: any, ...params:any) {
+    // Pass remaining arguments (including vendor specific ones) to parent constructor
+    super(...params);
+
+    // Maintains proper stack trace for where our error was thrown (only available on V8)
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, HttpError);
+    }
+
+    // Custom debugging information
+    this.data = data;
+    this.date = new Date();
+  }
+}
