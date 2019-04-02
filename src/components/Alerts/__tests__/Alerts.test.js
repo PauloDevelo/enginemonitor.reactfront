8f068798575b2a61d7db6d001cf0d60bcaaaa5d3
Torruella, Paul <paul.torruella@suez.com>
2019-04-02 20:16:48 +0800

    Ad the first tests using Enzyme for Alerts component

---
+++ src/components/Alerts/__tests__/Alerts.test.js
@@ -0,0 +1,39 @@
+import React, { Fragment } from 'react';
+import { shallow, mount } from 'enzyme';
+
+import Alerts from '../Alerts'
+
+describe('Component Alert', () =>{
+    it('should render even without props', () => {
+        const wrapper = shallow(<Alerts />);
+
+        expect(wrapper).toContainExactlyOneMatchingElement("Fragment");
+        expect(wrapper.contains(<Fragment></Fragment>)).toEqual(true);
+    });
+
+    it('should render with a message and the default color', () => {
+        const message = "test message";
+        const wrapper = mount(<Alerts error={message}/>);
+
+        expect(wrapper.text()).toEqual(message);
+        expect(wrapper.find('div').hasClass('alert')).toEqual(true);
+        expect(wrapper.find('div').hasClass('alert-danger')).toEqual(true);
+    });
+
+    it('should render with a message and a secondary color', () => {
+        const message = "test message";
+        const wrapper = mount(<Alerts error={message} color="secondary"/>);
+
+        expect(wrapper.text()).toEqual(message);
+        expect(wrapper.find('div').hasClass('alert')).toEqual(true);
+        expect(wrapper.find('div').hasClass('alert-secondary')).toEqual(true);
+    });
+
+    it('should render with a composed message and a danger color', () => {
+        const wrapper = mount(<Alerts errors={ {email: 'isrequired'} } />);
+
+        expect(wrapper.text()).toEqual('Email is required');
+        expect(wrapper.find('div').hasClass('alert')).toEqual(true);
+        expect(wrapper.find('div').hasClass('alert-danger')).toEqual(true);
+    });
+});
