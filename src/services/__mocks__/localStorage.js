c32c5e51aa13260443b5ad2ce5965afc8100f37c
Torruella, Paul <paul.torruella@suez.com>
2019-04-02 12:13:31 +0800

    5 first jest units for EquipmentMonitorServiceProxy

---
+++ src/services/__mocks__/localStorage.js
@@ -0,0 +1,7 @@
+const mockStorage = {};
+
+module.exports = window.localStorage = {
+  setItem: (key, val) => Object.assign(mockStorage, {[key]: val}),
+  getItem: (key) => mockStorage[key],
+  clear: () => mockStorage = {}
+};
