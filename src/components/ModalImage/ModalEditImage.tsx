a32a29cce5245b45d200e2693d41e0e8c18283c8
Torruella, Paul <paul.torruella@suez.com>
2019-09-13 23:04:02 +0800

    Add a modal to edit an image

---
+++ src/components/ModalImage/ModalEditImage.tsx
@@ -0,0 +1,72 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FormattedMessage, Messages, defineMessages } from 'react-intl';
+import { CSSTransition } from 'react-transition-group'
+
+import { useEditModalLogic } from '../../hooks/EditModalLogicHook';
+
+import jsonMessages from "./ModalEditImage.messages.json";
+const editImageMsg: Messages = defineMessages(jsonMessages);
+
+import imageProxy from '../../services/ImageProxy';
+
+import ModalYesNoConfirmation from '../ModalYesNoConfirmation/ModalYesNoConfirmation'
+import MyForm from "../Form/MyForm"
+import MyInput from "../Form/MyInput"
+import Alerts from "../Alerts/Alerts"
+import ActionButton from '../ActionButton/ActionButton';
+
+import '../../style/transition.css';
+import { ImageModel } from '../../types/Types';
+
+type Props = {
+    image: ImageModel | undefined, 
+    visible: boolean,
+	onImageSaved: (image: ImageModel) => void, 
+	toggle: () => void, 
+	onImageDeleted?: (image: ImageModel)=> void,
+	className?: string 
+}
+
+const ModalEditImage = ({image, visible, onImageSaved, toggle, onImageDeleted, className}: Props) => {
+	
+	const modalLogic = useEditModalLogic(toggle, imageProxy.updateImage, [], (image) => {}, onImageSaved, 
+                                            imageProxy.deleteImage, [], onImageDeleted);
+
+	return (
+		<Fragment>
+			<CSSTransition in={visible} timeout={300} classNames="modal">
+				<Modal isOpen={visible} toggle={modalLogic.cancel} className={className} fade={false}>
+					<ModalHeader toggle={modalLogic.cancel}><FontAwesomeIcon icon={faEdit} />{' '}<FormattedMessage {...editImageMsg.modalImageEditTitle} /></ModalHeader>
+					<ModalBody>
+						{image !== undefined && <img src={image.url} style={{width:"100%"}}/>}
+						{visible && 
+                        <MyForm id="imageEditForm" submit={modalLogic.handleSubmit} initialData={image}>
+							<MyInput name="title" 				label={editImageMsg.title} 				type="text"/>
+							<MyInput name="description" 	    label={editImageMsg.description} 	    type="text"/>
+						</MyForm>}
+						<Alerts errors={modalLogic.alerts}/>
+					</ModalBody>
+					<ModalFooter>
+						<ActionButton type="submit" isActing={modalLogic.isSaving} form="imageEditForm" color="success" message={editImageMsg.save}/>
+						<Button color="secondary" onClick={modalLogic.cancel}><FormattedMessage {...editImageMsg.cancel} /></Button>
+						<Button color="danger" onClick={modalLogic.handleDelete}><FormattedMessage {...editImageMsg.delete} /></Button>
+					</ModalFooter>
+				</Modal>
+			</CSSTransition>
+			<ModalYesNoConfirmation visible={modalLogic.yesNoModalVisibility}
+									toggle={modalLogic.toggleModalYesNoConfirmation}
+									yes={modalLogic.yesDelete}
+									isActing={modalLogic.isDeleting}
+									no={modalLogic.toggleModalYesNoConfirmation}
+									title={editImageMsg.imageDeleteTitle}
+									message={editImageMsg.imageDeleteMsg} 
+									className='modal-dialog-centered'/>
+		</Fragment>
+	);
+}
+
+export default ModalEditImage;
