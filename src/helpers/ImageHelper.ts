a60add0337173dba5245afe0d3d1cb5dd40b9e33
Torruella, Paul <paul.torruella@suez.com>
2019-09-14 22:23:33 +0800

    Rotate the image as per the Exif  orientation tag

--- src/helpers/ImageHelper.ts
+++ src/helpers/ImageHelper.ts
@@ -4,9 +4,11 @@ import imageProxy from '../services/ImageProxy';
 import Resizer from 'react-image-file-resizer';
 import { ImageModel } from '../types/Types';
 
+import {create} from "exif-parser";
+
 const compressionQuality = 95;
 
-const resizeImageIntoBlob = (imageFile: File, maxWidth: number, maxHeight: number):Promise<string> => {
+const resizeImageIntoBlob = (imageFile: File, maxWidth: number, maxHeight: number, orientationInDegrees: number):Promise<string> => {
     return new Promise(function(resolve, reject) {
         Resizer.imageFileResizer(
             imageFile,
@@ -14,7 +16,7 @@ const resizeImageIntoBlob = (imageFile: File, maxWidth: number, maxHeight: numbe
             maxHeight,
             'JPEG', // is the compressFormat of the  new image
             compressionQuality,
-            0, // is the rotation of the  new image
+            orientationInDegrees, // is the rotation of the  new image
             (blob: any) => { resolve(blob); },  // is the callBack function of the new image URI
             'blob'  // is the output type of the new image
         );
@@ -79,8 +81,11 @@ const resizeBase64ImageIntoBlob = (base64Str: string, maxWidth = 400, maxHeight 
 }
 
 export const resizeAndSaveImage = async (file: File, parentUiId: string):Promise<ImageModel> => {
-    const resizedBlob = await resizeImageIntoBlob(file, 1024, 1024);
-    const thumbnailBlob = await resizeImageIntoBlob(file, 100, 100);
+    const tags = await readExifTags(file);
+    const orientationInDegrees = convertExifOrientationIntoDegree(tags["Orientation"]);
+    
+    const resizedBlob = await resizeImageIntoBlob(file, 1024, 1024, orientationInDegrees);
+    const thumbnailBlob = await resizeImageIntoBlob(file, 100, 100, orientationInDegrees);
 
     const imgFormObj = new FormData();
         imgFormObj.append("name", file.name);
@@ -90,6 +95,45 @@ export const resizeAndSaveImage = async (file: File, parentUiId: string):Promise
         imgFormObj.append("_uiId", uuidv1());
 
     return await imageProxy.createImage(imgFormObj);
+}
+
+const readExifTags= async (file: File):Promise<any> => {
+    return new Promise(function(resolve, reject) {
+        const fileReader = new FileReader();
+        fileReader.onloadend = (ev:ProgressEvent) => {
+            var parser = create(fileReader.result as unknown as Buffer);
+            try {
+                var result = parser.parse();
+                resolve(result.tags);
+            } catch(err) {
+                reject(err);
+            }
+        };
+        fileReader.readAsArrayBuffer(file);
+    });
+}
+
+const convertExifOrientationIntoDegree = (exifOrientation: number): number => {
+    switch(exifOrientation){
+        case 1:
+            return 0;
+        case 2:
+            return 0;
+        case 3:
+            return 180;
+        case 4:
+            return 180;
+        case 5:
+            return 90;
+        case 6:
+            return 90;
+        case 7:
+            return 270;
+        case 8:
+            return 270;
+        default:
+            return 0;
+    }
 }
 
 export default resizeAndSaveImage;
