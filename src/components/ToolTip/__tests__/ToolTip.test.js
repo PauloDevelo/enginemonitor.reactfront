afbbe1fe041298608ede9f78b7156ab8de9e912c
Torruella, Paul <paul.torruella@suez.com>
2019-10-08 08:44:25 +0800

    Add uts for the Tooltip component

---
+++ src/components/ToolTip/__tests__/ToolTip.test.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { UncontrolledTooltip } from 'reactstrap';
+import { FormattedMessage } from 'react-intl';
+
+import {  mount, shallow } from 'enzyme';
+
+import ToolTip from '../ToolTip'
+
+describe('Component ToolTip', () =>{
+    let element;
+    let container;
+    let target;
+    let innerTarget;
+
+    beforeEach(() => {
+        element = document.createElement('div');
+        container = document.createElement('div');
+        element.innerHTML = '<p id="target">This is the tooltip <span id="innerTarget">target</span>.</p>';
+        element.setAttribute('id', 'testContainer');
+        container.setAttribute('id', 'container');
+        element.appendChild(container);
+        document.body.appendChild(element);
+        target = document.getElementById('target');
+        innerTarget = document.getElementById('innerTarget');
+    
+        jest.useFakeTimers();
+      });
+
+    it('should render as a simple tooltip', () => {
+        // Arrange
+        const message = { id:"tooltip", defaultMessage: "hello world", description: "a tooltip"};
+
+        // Act
+        const wrapper = mount(<ToolTip tooltip={message}/>, { attachTo: container });
+
+        // Assert
+        const innerToolTip = wrapper.find(UncontrolledTooltip);
+        const formatedMessage = innerToolTip.props().children;
+    
+        expect(formatedMessage.props.defaultMessage).toEqual(message.defaultMessage);
+        expect(formatedMessage.props.id).toEqual(message.id);
+    });
+
+    it('should be the same as the previous snapshot', () => {
+        // Arrange
+        const message = { id:"tooltip", defaultMessage: "hello world", description: "a tooltip"};
+
+        // Act
+        const wrapper = mount(<ToolTip tooltip={message}/>, { attachTo: container });
+
+        // Assert
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    
+});
