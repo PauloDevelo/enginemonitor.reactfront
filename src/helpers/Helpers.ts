2a8e79ac5f56772a0e86449e459e9638166f3aeb
Torruella, Paul <paul.torruella@suez.com>
2019-11-12 09:10:46 +0800

    Helpers eslinted

--- src/helpers/Helpers.ts
+++ src/helpers/Helpers.ts
@@ -1,24 +1,23 @@
-export function formatDateInUTC(date: Date): string{
-	return "" + date.getUTCFullYear() + "-" + (date.getUTCMonth()+1) + "-" + date.getUTCDate() + "T00:00:00+0000";
+export function formatDateInUTC(date: Date): string {
+  return `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}T00:00:00+0000`;
 }
 
 export function scrollTo(left: number, top: number, scrollDuration: number) {
-	const minimumStepInMs = 5;
-	const nbSteps = scrollDuration / minimumStepInMs;
+  const minimumStepInMs = 5;
+  const nbSteps = scrollDuration / minimumStepInMs;
 
-	var scrollStepY = (top - window.scrollY) / nbSteps;
-	var scrollStepX = (left - window.scrollX) / nbSteps;
+  const scrollStepY = (top - window.scrollY) / nbSteps;
+  const scrollStepX = (left - window.scrollX) / nbSteps;
 
-	let counter = 0;
-	var scrollInterval = setInterval(
-		() => {
-			if(counter++ <= nbSteps){
-				window.scrollBy( scrollStepX, scrollStepY );
-			}
-			else{
-				clearInterval(scrollInterval);
-			}
-		},
-		minimumStepInMs
-	);
+  let counter = 0;
+  const scrollInterval = setInterval(
+    () => {
+      if (counter++ <= nbSteps) {
+        window.scrollBy(scrollStepX, scrollStepY);
+      } else {
+        clearInterval(scrollInterval);
+      }
+    },
+    minimumStepInMs,
+  );
 }
