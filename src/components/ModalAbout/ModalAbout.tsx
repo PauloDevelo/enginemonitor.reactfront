d183b5583e29f5fbd86bc076605218149af49f75
Torruella, Paul <paul.torruella@suez.com>
2019-11-12 09:10:26 +0800

    ModalAbout eslinted

--- src/components/ModalAbout/ModalAbout.tsx
+++ src/components/ModalAbout/ModalAbout.tsx
@@ -1,52 +1,47 @@
 import React from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, Media } from 'reactstrap';
+import {
+  Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, Media,
+} from 'reactstrap';
 
 import { FormattedMessage, defineMessages } from 'react-intl';
-import PropTypes from 'prop-types';
 import { CSSTransition } from 'react-transition-group';
 
 import appVersion from '../../global';
 
-import jsonMessages from "./ModalAbout.messages.json";
+import jsonMessages from './ModalAbout.messages.json';
+
 const aboutMsg = defineMessages(jsonMessages);
 
 type Props = {
-	visible: boolean, 
-	className?: string, 
-	toggle: ()=>void
+visible: boolean;
+className?: string;
+toggle: ()=>void;
 }
 
-const ModalAbout = ({ visible, className, toggle }: Props) => {
-	return (
-			<CSSTransition in={visible} timeout={300} classNames="modal">
-				<Modal isOpen={visible} toggle={toggle} className={className} fade={false}>
-					<ModalHeader toggle={toggle}>
-                        <Media left>
-                            <Media object src="./images/engine48.png" alt="Equipment Maintenance icon" />
-                        </Media>
-                        <Media body>
-                            <Media heading>
-                                <FormattedMessage {...aboutMsg.title} />&nbsp;v{appVersion}
-                            </Media>
-							<FormattedMessage {...aboutMsg.about} />
-                        </Media>
-					</ModalHeader>
-					<ModalBody>
-						{visible && <Form id="aboutForm">
-						</Form>}
-					</ModalBody>
-					<ModalFooter>
-						<Button type="submit" form="aboutForm" color="success" onClick={toggle}><FormattedMessage {...aboutMsg.ok} /></Button>
-					</ModalFooter>
-				</Modal>
-			</CSSTransition>
-	);
-}
-
-ModalAbout.propTypes = {
-	visible: PropTypes.bool.isRequired,
-	className: PropTypes.string,
-	toggle: PropTypes.func.isRequired
-};
+const ModalAbout = ({ visible, className, toggle }: Props) => (
+  <CSSTransition in={visible} timeout={300} classNames="modal">
+    <Modal isOpen={visible} toggle={toggle} className={className} fade={false}>
+      <ModalHeader toggle={toggle}>
+        <Media left>
+          <Media object src="./images/engine48.png" alt="Equipment Maintenance icon" />
+        </Media>
+        <Media body>
+          <Media heading>
+            <FormattedMessage {...aboutMsg.title} />
+&nbsp;v
+            {appVersion}
+          </Media>
+          <FormattedMessage {...aboutMsg.about} />
+        </Media>
+      </ModalHeader>
+      <ModalBody>
+        {visible && (<Form id="aboutForm" />)}
+      </ModalBody>
+      <ModalFooter>
+        <Button type="submit" form="aboutForm" color="success" onClick={toggle}><FormattedMessage {...aboutMsg.ok} /></Button>
+      </ModalFooter>
+    </Modal>
+  </CSSTransition>
+);
 
 export default ModalAbout;
