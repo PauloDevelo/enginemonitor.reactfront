fdb962c1919b3ee6d4a08d713ae88849c1e5af54
Torruella, Paul <paul.torruella@suez.com>
2019-11-15 09:42:48 +0800

    eslint

--- src/services/ActionManager.ts
+++ src/services/ActionManager.ts
@@ -1,8 +1,14 @@
+/* eslint-disable max-classes-per-file */
+
 import httpProxy from './HttpProxy';
+
+// eslint-disable-next-line no-unused-vars
 import storageService, { IUserStorageListener } from './StorageService';
 
 export enum ActionType{
+    // eslint-disable-next-line no-unused-vars
     Post,
+    // eslint-disable-next-line no-unused-vars
     Delete
 }
 
@@ -10,7 +16,7 @@ export interface Action{
     type: ActionType,
     key:string,
     data?:any
-};
+}
 
 export interface IActionManager{
     addAction(action: Action): Promise<void>;
@@ -24,105 +30,100 @@ export interface IActionManager{
     unregisterOnActionManagerChanged(listenerToRemove: (actionCounter: number) => void):void;
 }
 
-export class NoActionPendingError extends Error{};
+export class NoActionPendingError extends Error {}
 
-class ActionManager implements IActionManager, IUserStorageListener{
-
+class ActionManager implements IActionManager, IUserStorageListener {
     private listeners: ((actionCounter: number) => void)[] = [];
 
-    constructor(){
-        storageService.registerUserStorageListener(this);
+    constructor() {
+      storageService.registerUserStorageListener(this);
     }
 
     registerOnActionManagerChanged(listener: (actionCounter: number) => void):void{
-        this.listeners.push(listener);
+      this.listeners.push(listener);
     }
 
     unregisterOnActionManagerChanged(listenerToRemove: (actionCounter: number) => void):void{
-        this.listeners = this.listeners.filter(listener => listener !== listenerToRemove);
+      this.listeners = this.listeners.filter((listener) => listener !== listenerToRemove);
     }
 
-    async onUserStorageOpened():Promise<void>{
-        const nbAction = await this.countAction();
-        this.triggerOnActionManagerChanged(nbAction);
+    async onUserStorageOpened():Promise<void> {
+      const nbAction = await this.countAction();
+      this.triggerOnActionManagerChanged(nbAction);
     }
 
-    async onUserStorageClosed():Promise<void>{
-        this.triggerOnActionManagerChanged(0);
+    async onUserStorageClosed():Promise<void> {
+      this.triggerOnActionManagerChanged(0);
     }
 
-    private async triggerOnActionManagerChanged(actionCounter: number): Promise<void>{
-        this.listeners.map(listener => listener(actionCounter));
+    private async triggerOnActionManagerChanged(actionCounter: number): Promise<void> {
+      this.listeners.map((listener) => listener(actionCounter));
     }
 
-    async addAction(action: Action): Promise<void>{
-        let history:Action[] = await this.getHistoryFromStorage();
+    async addAction(action: Action): Promise<void> {
+      const history:Action[] = await this.getHistoryFromStorage();
 
-        history.push(action);
+      history.push(action);
 
-        var actions = await storageService.setItem<Action[]>("history", history);
-        this.triggerOnActionManagerChanged(actions.length);
+      const actions = await storageService.setItem<Action[]>('history', history);
+      this.triggerOnActionManagerChanged(actions.length);
     }
 
-    async getNextActionToPerform(): Promise<Action>{
-        let action: Action | undefined;
-        let history:Action[] = await this.getHistoryFromStorage();
+    async getNextActionToPerform(): Promise<Action> {
+      const history:Action[] = await this.getHistoryFromStorage();
+      const action = history.shift();
 
-        action = history.shift();
+      if (!action) {
+        throw new NoActionPendingError("There isn't pending action anymore");
+      }
 
-        if(!action)
-        {
-            throw new NoActionPendingError("There isn't pending action anymore");
-        }
+      const actions = await storageService.setItem<Action[]>('history', history);
+      this.triggerOnActionManagerChanged(actions.length);
 
-        var actions = await storageService.setItem<Action[]>("history", history);
-        this.triggerOnActionManagerChanged(actions.length);
-
-        return action;
+      return action;
     }
 
     async putBackAction(action: Action): Promise<void> {
-        let newHistory: Action[] = [];
-        newHistory.push(action);
+      let newHistory: Action[] = [];
+      newHistory.push(action);
 
-        let  history:Action[] = await this.getHistoryFromStorage();
+      const history:Action[] = await this.getHistoryFromStorage();
 
-        newHistory = newHistory.concat(history);
+      newHistory = newHistory.concat(history);
 
-        var actions = await storageService.setItem<Action[]>("history", newHistory);
-        this.triggerOnActionManagerChanged(actions.length);
+      const actions = await storageService.setItem<Action[]>('history', newHistory);
+      this.triggerOnActionManagerChanged(actions.length);
     }
 
-    async countAction(): Promise<number>{
-        if(storageService.isUserStorageOpened() === false){
-            return 0;
-        }
-        
-        return (await storageService.getArray<Action>("history")).length;
+    countAction = async (): Promise<number> => {
+      if (storageService.isUserStorageOpened() === false) {
+        return 0;
+      }
+
+      const actions = await storageService.getArray<Action>('history');
+      return actions.length;
     }
 
     performAction = async (action: Action):Promise<void> => {
-        if(action.type === ActionType.Post){
-            await httpProxy.post(action.key, action.data);
-        }
-        else if(action.type === ActionType.Delete){
-            await httpProxy.deleteReq(action.key);
-        }
-        else{
-            throw new Error("The action type " + action.type + " is not recognized.");
-        }
+      if (action.type === ActionType.Post) {
+        await httpProxy.post(action.key, action.data);
+      } else if (action.type === ActionType.Delete) {
+        await httpProxy.deleteReq(action.key);
+      } else {
+        throw new Error(`The action type ${action.type} is not recognized.`);
+      }
     }
 
-    async clearActions(): Promise<void>{
-        var actions = await storageService.setItem<Action[]>("history", []);
-        this.triggerOnActionManagerChanged(actions.length);
+    async clearActions(): Promise<void> {
+      const actions = await storageService.setItem<Action[]>('history', []);
+      this.triggerOnActionManagerChanged(actions.length);
     }
 
-    private async getHistoryFromStorage():Promise<Action[]>{
-        let  history:Action[] = await storageService.getItem<Action[]>("history");
-        history = history ? history : [];
+    private getHistoryFromStorage = async ():Promise<Action[]> => {
+      let history:Action[] = await storageService.getItem<Action[]>('history');
+      history = history || [];
 
-        return history;
+      return history;
     }
 }
 
