f4767584f13f70a7da92bca79d53687461788206
Torruella, Paul <paul.torruella@suez.com>
2019-11-15 09:48:58 +0800

    eslint

--- src/services/__tests__/SyncService.test.js
+++ src/services/__tests__/SyncService.test.js
@@ -180,9 +180,13 @@ describe('Test SyncService', () => {
       // Assert
       expect(syncListener).toHaveBeenCalledTimes(4);
 
+      // eslint-disable-next-line max-len
       expect(contexts[0]).toEqual({ isSyncing: true, totalActionToSync: 2, remainingActionToSync: 2 });
+      // eslint-disable-next-line max-len
       expect(contexts[1]).toEqual({ isSyncing: true, totalActionToSync: 2, remainingActionToSync: 1 });
+      // eslint-disable-next-line max-len
       expect(contexts[2]).toEqual({ isSyncing: true, totalActionToSync: 2, remainingActionToSync: 0 });
+      // eslint-disable-next-line max-len
       expect(contexts[3]).toEqual({ isSyncing: false, totalActionToSync: 2, remainingActionToSync: 0 });
 
       expect(getNextActionToPerform).toBeCalledTimes(3);
