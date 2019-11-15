e199492a5e5ea3645639c78aa842d0d1dd5985d3
Torruella, Paul <paul.torruella@suez.com>
2019-11-16 07:49:31 +0800

    eslint + init log

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
+import * as log from 'loglevel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { IntlProvider } from "react-intl";
+import { IntlProvider } from 'react-intl';
 
 import './index.css';
 
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
-import messages_fr from "./translations/fr.json";
-import messages_en from "./translations/en.json";
+import messagesFr from './translations/fr.json';
+import messagesEn from './translations/en.json';
 
 const messages = {
-    'fr': messages_fr,
-    'en': messages_en
+  fr: messagesFr,
+  en: messagesEn,
 };
 
-const language = navigator.language
-const shortLanguage = navigator.language.split(/[-_]/)[0];  // language without region code
+const { language } = navigator;
+const shortLanguage = navigator.language.split(/[-_]/)[0]; // language without region code
 
+log.setLevel('trace', false);
 
-ReactDOM.render(<IntlProvider locale={language} messages={messages[shortLanguage]}>
-					<App />
-				</IntlProvider>, 
-				document.getElementById('root'));
+ReactDOM.render(<IntlProvider locale={language} messages={messages[shortLanguage]}><App /></IntlProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
