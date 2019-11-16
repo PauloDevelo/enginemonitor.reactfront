b0ac727e11a99328ec11d7d1c886cc1e7ac97bf5
Torruella, Paul <paul.torruella@suez.com>
2019-11-16 09:47:18 +0800

    eslint

--- src/testHelpers/MockConsole.ts
+++ src/testHelpers/MockConsole.ts
@@ -1,16 +1,18 @@
+/* eslint-env jest */
+/* eslint-disable no-console */
 const ignoredMessages:string[] = [
 ];
 
-const mockConsoleMethod = (realConsoleMethod: (message?: any, ...args: any[])=> void) => {
-    return (message?: any, ...args: any[]) => {
-        const containsIgnoredMessage = ignoredMessages.some((ignoredMessage) => message.toString().includes(ignoredMessage));
+// eslint-disable-next-line max-len
+const mockConsoleMethod = (realConsoleMethod: (message?: any, ...args: any[])=> void) => (message?: any, ...args: any[]) => {
+  // eslint-disable-next-line max-len
+  const containsIgnoredMessage = ignoredMessages.some((ignoredMessage) => message.toString().includes(ignoredMessage));
 
-        if (!containsIgnoredMessage) {
-            realConsoleMethod(message, ...args);
-        }
-    };
+  if (!containsIgnoredMessage) {
+    realConsoleMethod(message, ...args);
+  }
 };
-  
+
 // Suppress console errors and warnings to avoid polluting output in tests.
 console.warn = jest.fn(mockConsoleMethod(console.warn));
 console.log = jest.fn(mockConsoleMethod(console.log));
