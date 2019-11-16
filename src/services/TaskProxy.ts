6ef8cc183c527d006e84378f1c9b830c5aedd9e6
Torruella, Paul <paul.torruella@suez.com>
2019-11-16 09:50:02 +0800

    eslint

--- src/services/TaskProxy.ts
+++ src/services/TaskProxy.ts
@@ -1,3 +1,6 @@
+import * as log from 'loglevel';
+// eslint-disable-next-line no-unused-vars
+import { CancelToken } from 'axios';
 import progressiveHttpProxy from './ProgressiveHttpProxy';
 
 import entryProxy from './EntryProxy';
@@ -6,8 +9,8 @@ import imageProxy from './ImageProxy';
 import storageService from './StorageService';
 
 import { updateTask, updateRealtimeFields } from '../helpers/TaskHelper';
-import { TaskModel} from '../types/Types';
-import { CancelToken } from 'axios';
+// eslint-disable-next-line no-unused-vars
+import { TaskModel } from '../types/Types';
 
 export interface FetchTaskProps{
     equipmentId: string | undefined;
@@ -25,66 +28,68 @@ export interface ITaskProxy{
     onEquipmentDeleted(equipmentId: string): Promise<void>;
 }
 
-class TaskProxy implements ITaskProxy{
-    baseUrl = process.env.REACT_APP_URL_BASE + "tasks/";
+class TaskProxy implements ITaskProxy {
+    baseUrl = `${process.env.REACT_APP_URL_BASE}tasks/`;
 
-    /////////////////Task////////////////////////////
-    createOrSaveTask = async (equipmentId: string, newTask: TaskModel):Promise<TaskModel> =>{
-        newTask = await progressiveHttpProxy.postAndUpdate<TaskModel>(this.baseUrl + equipmentId + '/' + newTask._uiId, "task", newTask, updateTask);
+    // ///////////////Task////////////////////////////
+    createOrSaveTask = async (equipmentId: string, newTask: TaskModel):Promise<TaskModel> => {
+      const updatedTask = await progressiveHttpProxy.postAndUpdate<TaskModel>(`${this.baseUrl + equipmentId}/${newTask._uiId}`, 'task', newTask, updateTask);
 
-        await storageService.updateArray(this.baseUrl + equipmentId, newTask);
+      await storageService.updateArray(this.baseUrl + equipmentId, updatedTask);
 
-        return newTask;
+      return updatedTask;
     }
 
-    deleteTask = async(equipmentId: string, taskId: string): Promise<TaskModel> => {
-        await progressiveHttpProxy.deleteAndUpdate<TaskModel>(this.baseUrl + equipmentId + '/' + taskId, "task", updateTask);
+    deleteTask = async (equipmentId: string, taskId: string): Promise<TaskModel> => {
+      await progressiveHttpProxy.deleteAndUpdate<TaskModel>(`${this.baseUrl + equipmentId}/${taskId}`, 'task', updateTask);
 
-        const removedTask = await storageService.removeItemInArray<TaskModel>(this.baseUrl + equipmentId, taskId);
-        await entryProxy.onTaskDeleted(equipmentId, taskId);
-        await imageProxy.onEntityDeleted(taskId);
+      // eslint-disable-next-line max-len
+      const removedTask = await storageService.removeItemInArray<TaskModel>(this.baseUrl + equipmentId, taskId);
+      await entryProxy.onTaskDeleted(equipmentId, taskId);
+      await imageProxy.onEntityDeleted(taskId);
 
-        return updateTask(removedTask);
+      return updateTask(removedTask);
     }
 
-    fetchTasks = async({equipmentId, forceToLookUpInStorage = false, cancelToken = undefined} :FetchTaskProps): Promise<TaskModel[]> => {
-        if(equipmentId === undefined)
-        {
-            return [];
-        }
+    // eslint-disable-next-line max-len
+    fetchTasks = async ({ equipmentId, forceToLookUpInStorage = false, cancelToken = undefined } :FetchTaskProps): Promise<TaskModel[]> => {
+      if (equipmentId === undefined) {
+        return [];
+      }
 
-        if(forceToLookUpInStorage){
-            return await storageService.getArray(this.baseUrl + equipmentId);
-        }
+      if (forceToLookUpInStorage) {
+        return storageService.getArray(this.baseUrl + equipmentId);
+      }
 
-        const tasks:TaskModel[] = await progressiveHttpProxy.getArrayOnlineFirst(this.baseUrl + equipmentId, "tasks", updateTask, cancelToken);
-        await Promise.all(tasks.map(async (task) => {
-            await updateRealtimeFields(equipmentId, task);
-        }));
+      const tasks:TaskModel[] = await progressiveHttpProxy.getArrayOnlineFirst(this.baseUrl + equipmentId, 'tasks', updateTask, cancelToken);
+      await Promise.all(tasks.map(async (task) => {
+        await updateRealtimeFields(equipmentId, task);
+      }));
 
-        return tasks;
+      return tasks;
     }
 
+    // eslint-disable-next-line max-len
     existTask = async (equipmentId: string | undefined, taskId: string | undefined):Promise<boolean> => {
-        if (equipmentId === undefined || taskId === undefined){
-            console.error("The function TaskProxy.existTask expects a non null and non undefined task id.")
-            return false;
-        }
+      if (equipmentId === undefined || taskId === undefined) {
+        log.error('The function TaskProxy.existTask expects a non null and non undefined task id.');
+        return false;
+      }
 
-        const allTasks = await this.fetchTasks({equipmentId, forceToLookUpInStorage:true});
+      const allTasks = await this.fetchTasks({ equipmentId, forceToLookUpInStorage: true });
 
-        return allTasks.findIndex(task => task._uiId === taskId) !== -1;
+      return allTasks.findIndex((task) => task._uiId === taskId) !== -1;
     }
 
     onEquipmentDeleted = async (equipmentId: string): Promise<void> => {
-        var tasks = await this.fetchTasks({equipmentId, forceToLookUpInStorage:true});
+      const tasks = await this.fetchTasks({ equipmentId, forceToLookUpInStorage: true });
 
-        await tasks.reduce(async (previousPromise, task) => {
-            await previousPromise;
-            await storageService.removeItemInArray<TaskModel>(this.baseUrl + equipmentId, task._uiId);
-            await entryProxy.onTaskDeleted(equipmentId, task._uiId);
-            await imageProxy.onEntityDeleted(task._uiId);
-        }, Promise.resolve());
+      await tasks.reduce(async (previousPromise, task) => {
+        await previousPromise;
+        await storageService.removeItemInArray<TaskModel>(this.baseUrl + equipmentId, task._uiId);
+        await entryProxy.onTaskDeleted(equipmentId, task._uiId);
+        await imageProxy.onEntityDeleted(task._uiId);
+      }, Promise.resolve());
     }
 }
 
