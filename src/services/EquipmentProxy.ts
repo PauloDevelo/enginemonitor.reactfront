85c1f7d40c4fd04f1d17e074e2418591a5ec54df
Torruella, Paul <paul.torruella@suez.com>
2019-11-16 09:26:54 +0800

    eslint + console

--- src/services/EquipmentProxy.ts
+++ src/services/EquipmentProxy.ts
@@ -1,3 +1,4 @@
+import * as log from 'loglevel';
 import progressiveHttpProxy from './ProgressiveHttpProxy';
 
 import storageService from './StorageService';
@@ -5,8 +6,9 @@ import storageService from './StorageService';
 import taskProxy from './TaskProxy';
 import entryProxy from './EntryProxy';
 
-import { updateEquipment } from '../helpers/EquipmentHelper'
-import { EquipmentModel} from '../types/Types'
+import { updateEquipment } from '../helpers/EquipmentHelper';
+// eslint-disable-next-line no-unused-vars
+import { EquipmentModel } from '../types/Types';
 import imageProxy from './ImageProxy';
 
 export interface IEquipmentProxy{
@@ -19,46 +21,47 @@ export interface IEquipmentProxy{
     existEquipment(equipmentId: string | undefined):Promise<boolean>;
 }
 
-class EquipmentProxy implements IEquipmentProxy{
-    private baseUrl:string = process.env.REACT_APP_URL_BASE + "equipments/";
+class EquipmentProxy implements IEquipmentProxy {
+    private baseUrl:string = `${process.env.REACT_APP_URL_BASE}equipments/`;
 
-    ////////////////Equipment////////////////////////
-    fetchEquipments = async(forceToLookUpInStorage: boolean = false): Promise<EquipmentModel[]> => {
-        if (forceToLookUpInStorage){
-            return await storageService.getArray<EquipmentModel>(this.baseUrl)
-        }
-        
-        return await progressiveHttpProxy.getArrayOnlineFirst<EquipmentModel>(this.baseUrl, "equipments", updateEquipment);
+    // //////////////Equipment////////////////////////
+    // eslint-disable-next-line max-len
+    fetchEquipments = async (forceToLookUpInStorage: boolean = false): Promise<EquipmentModel[]> => {
+      if (forceToLookUpInStorage) {
+        return storageService.getArray<EquipmentModel>(this.baseUrl);
+      }
+
+      return progressiveHttpProxy.getArrayOnlineFirst<EquipmentModel>(this.baseUrl, 'equipments', updateEquipment);
     }
-    
-    createOrSaveEquipment = async(equipmentToSave: EquipmentModel):Promise<EquipmentModel> => {
-        equipmentToSave = await progressiveHttpProxy.postAndUpdate<EquipmentModel>(this.baseUrl + equipmentToSave._uiId, "equipment", equipmentToSave, updateEquipment);           
-        await storageService.updateArray(this.baseUrl, equipmentToSave);
-        return equipmentToSave;
+
+    createOrSaveEquipment = async (equipmentToSave: EquipmentModel):Promise<EquipmentModel> => {
+      const updatedEquipment = await progressiveHttpProxy.postAndUpdate<EquipmentModel>(this.baseUrl + equipmentToSave._uiId, 'equipment', equipmentToSave, updateEquipment);
+      await storageService.updateArray(this.baseUrl, updatedEquipment);
+      return updatedEquipment;
     }
 
     deleteEquipment = async (idEquipment: string): Promise<EquipmentModel> => {
-        await progressiveHttpProxy.deleteAndUpdate<EquipmentModel>(this.baseUrl + idEquipment, "equipment", updateEquipment);
+      await progressiveHttpProxy.deleteAndUpdate<EquipmentModel>(this.baseUrl + idEquipment, 'equipment', updateEquipment);
 
-        await entryProxy.onEquipmentDeleted(idEquipment);
-        await taskProxy.onEquipmentDeleted(idEquipment);
-        await imageProxy.onEntityDeleted(idEquipment);
+      await entryProxy.onEquipmentDeleted(idEquipment);
+      await taskProxy.onEquipmentDeleted(idEquipment);
+      await imageProxy.onEntityDeleted(idEquipment);
 
-        return updateEquipment(await storageService.removeItemInArray<EquipmentModel>(this.baseUrl, idEquipment));
+      // eslint-disable-next-line max-len
+      const deletedEquipment = await storageService.removeItemInArray<EquipmentModel>(this.baseUrl, idEquipment);
+      return updateEquipment(deletedEquipment);
     }
 
-    getStoredEquipment = async():Promise<EquipmentModel[]> => {
-        return this.fetchEquipments(true);
-    }
+    getStoredEquipment = async ():Promise<EquipmentModel[]> => this.fetchEquipments(true)
 
     existEquipment = async (equipmentId: string | undefined):Promise<boolean> => {
-        if (equipmentId === undefined){
-            console.error("The function EquipmentProxy.existEquipment expects a non null and non undefined equipment id.")
-            return false;
-        }
+      if (equipmentId === undefined) {
+        log.error('The function EquipmentProxy.existEquipment expects a non null and non undefined equipment id.');
+        return false;
+      }
 
-        const allEquipments = await this.fetchEquipments(true);
-        return allEquipments.findIndex(equipment => equipment._uiId === equipmentId) !== -1;
+      const allEquipments = await this.fetchEquipments(true);
+      return allEquipments.findIndex((equipment) => equipment._uiId === equipmentId) !== -1;
     }
 }
 
