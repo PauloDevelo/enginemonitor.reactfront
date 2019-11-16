99b2d8d20b622d01852058f9c6cae72825434a1f
Torruella, Paul <paul.torruella@suez.com>
2019-11-16 08:29:48 +0800

    log

--- src/hooks/Debug.ts
+++ src/hooks/Debug.ts
@@ -1,17 +1,21 @@
+import * as log from 'loglevel';
 import { useRef, useEffect } from 'react';
 
-export function useTraceUpdate(componentName: string, props: any) {
-    const prev = useRef(props);
-    useEffect(() => {
-      const changedProps = Object.entries(props).reduce((ps: any, [k, v]) => {
-        if (prev.current[k] !== v) {
-          ps[k] = [prev.current[k], v];
-        }
-        return ps;
-      }, {});
-      if (Object.keys(changedProps).length > 0) {
-        console.log(componentName + ' changed props:', changedProps);
+function useTraceUpdate(componentName: string, props: any) {
+  const prev = useRef(props);
+  useEffect(() => {
+    const changedProps = Object.entries(props).reduce((ps: any, [k, v]) => {
+      if (prev.current[k] !== v) {
+        // eslint-disable-next-line no-param-reassign
+        ps[k] = [prev.current[k], v];
       }
-      prev.current = props;
-    });
+      return ps;
+    }, {});
+    if (Object.keys(changedProps).length > 0) {
+      log.debug(`${componentName} changed props:`, changedProps);
+    }
+    prev.current = props;
+  });
 }
+
+export default useTraceUpdate;
