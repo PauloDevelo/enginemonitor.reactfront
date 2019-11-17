555b6dab40ebd1bf10c7a9eaa52cdcd07d5051e9
Torruella, Paul <paul.torruella@suez.com>
2019-11-17 09:59:54 +0800

    eslint

--- src/services/ImageProxy.ts
+++ src/services/ImageProxy.ts
@@ -1,12 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import { CancelToken } from 'axios';
 import progressiveHttpProxy from './ProgressiveHttpProxy';
 import httpProxy from './HttpProxy';
 
 import storageService from './StorageService';
 
-import { ImageModel } from '../types/Types'
+// eslint-disable-next-line no-unused-vars
+import { ImageModel } from '../types/Types';
 
 import userContext from './UserContext';
-import { CancelToken } from 'axios';
 
 export interface FetchImagesProps{
     parentUiId: string;
@@ -23,47 +25,47 @@ export interface IImageProxy{
     onEntityDeleted(parentUiId: string):Promise<void>;
 }
 
-class ImageProxy implements IImageProxy{
-    private baseUrl:string = process.env.REACT_APP_URL_BASE + "images/";
+class ImageProxy implements IImageProxy {
+    private baseUrl:string = `${process.env.REACT_APP_URL_BASE}images/`;
 
-    ////////////////Equipment////////////////////////
-    fetchImages = async({ parentUiId, forceToLookUpInStorage = false, cancelToken = undefined }: FetchImagesProps): Promise<ImageModel[]> => {
-        if (forceToLookUpInStorage){
-            return await storageService.getArray<ImageModel>(this.baseUrl + parentUiId);
-        }
-        
-        return await progressiveHttpProxy.getArrayOnlineFirst<ImageModel>(this.baseUrl + parentUiId, "images", (image) => image, cancelToken);
-    }
-    
-    createImage = async(imgFormObj: FormData):Promise<ImageModel> => {
-        const {image} = await httpProxy.post(this.baseUrl + imgFormObj.get('parentUiId'), imgFormObj);
-        await storageService.updateArray(this.baseUrl + image.parentUiId, image);
-        userContext.onImageAdded(image.sizeInByte);
-        return image;
+    // //////////////Equipment////////////////////////
+    fetchImages = async ({ parentUiId, forceToLookUpInStorage = false, cancelToken = undefined }: FetchImagesProps): Promise<ImageModel[]> => {
+      if (forceToLookUpInStorage) {
+        return storageService.getArray<ImageModel>(this.baseUrl + parentUiId);
+      }
+
+      return progressiveHttpProxy.getArrayOnlineFirst<ImageModel>(this.baseUrl + parentUiId, 'images', (image) => image, cancelToken);
     }
 
-    updateImage = async(imageToSave: ImageModel):Promise<ImageModel> => {
-        const updatedImage = await progressiveHttpProxy.postAndUpdate(this.baseUrl + imageToSave.parentUiId + "/" + imageToSave._uiId, "image", imageToSave, (image) => image);
-        await storageService.updateArray(this.baseUrl + updatedImage.parentUiId, updatedImage);
-        return updatedImage;
+    createImage = async (imgFormObj: FormData):Promise<ImageModel> => {
+      const { image } = await httpProxy.post(this.baseUrl + imgFormObj.get('parentUiId'), imgFormObj);
+      await storageService.updateArray(this.baseUrl + image.parentUiId, image);
+      userContext.onImageAdded(image.sizeInByte);
+      return image;
+    }
+
+    updateImage = async (imageToSave: ImageModel):Promise<ImageModel> => {
+      const updatedImage = await progressiveHttpProxy.postAndUpdate(`${this.baseUrl + imageToSave.parentUiId}/${imageToSave._uiId}`, 'image', imageToSave, (image) => image);
+      await storageService.updateArray(this.baseUrl + updatedImage.parentUiId, updatedImage);
+      return updatedImage;
     };
 
     deleteImage = async (image: ImageModel): Promise<ImageModel> => {
-        await progressiveHttpProxy.deleteAndUpdate<ImageModel>(this.baseUrl + image.parentUiId + "/" + image._uiId, "image", (image) => image);
-        const deletedImage = await storageService.removeItemInArray<ImageModel>(this.baseUrl + image.parentUiId, image._uiId);
-        userContext.onImageRemoved(deletedImage.sizeInByte);
+      await progressiveHttpProxy.deleteAndUpdate<ImageModel>(`${this.baseUrl + image.parentUiId}/${image._uiId}`, 'image', (anImage) => anImage);
+      const deletedImage = await storageService.removeItemInArray<ImageModel>(this.baseUrl + image.parentUiId, image._uiId);
+      userContext.onImageRemoved(deletedImage.sizeInByte);
 
-        return deletedImage;
+      return deletedImage;
     }
 
-    onEntityDeleted = async(parentUiId: string):Promise<void> => {
-        var images = await this.fetchImages({ parentUiId, forceToLookUpInStorage:true });
+    onEntityDeleted = async (parentUiId: string):Promise<void> => {
+      const images = await this.fetchImages({ parentUiId, forceToLookUpInStorage: true });
 
-        await images.reduce(async (previousPromise, image) => {
-            await previousPromise;
-            const deletedImage = await storageService.removeItemInArray<ImageModel>(this.baseUrl + image.parentUiId, image._uiId);
-            userContext.onImageRemoved(deletedImage.sizeInByte);
-        }, Promise.resolve());
+      await images.reduce(async (previousPromise, image) => {
+        await previousPromise;
+        const deletedImage = await storageService.removeItemInArray<ImageModel>(this.baseUrl + image.parentUiId, image._uiId);
+        userContext.onImageRemoved(deletedImage.sizeInByte);
+      }, Promise.resolve());
     }
 }
 
