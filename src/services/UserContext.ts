887d7908ed7a42bd2e5cb4e417eeeb572a811e76
Torruella, Paul <paul.torruella@suez.com>
2019-11-17 08:11:46 +0800

    eslint

--- src/services/UserContext.ts
+++ src/services/UserContext.ts
@@ -1,4 +1,5 @@
-import {UserModel} from '../types/Types'
+// eslint-disable-next-line no-unused-vars
+import { UserModel } from '../types/Types';
 
 export interface IUserContext{
     getCurrentUser():UserModel | undefined;
@@ -14,58 +15,61 @@ export interface IUserContext{
     unregisterOnUserChanged(listener: (newUser: UserModel | undefined) => void):void;
 }
 
-class UserContext implements IUserContext{
+class UserContext implements IUserContext {
     private user:UserModel | undefined = undefined;
+
     private userStorageSizeListeners: ((newUserStorageSize: number) => void)[] = [];
+
     private userListeners: ((newUser: UserModel | undefined) => void)[] = [];
 
-    getCurrentUser():UserModel | undefined{
-        return this.user;
+    getCurrentUser():UserModel | undefined {
+      return this.user;
     }
 
-    onUserChanged(user: UserModel | undefined){
-        this.user = user;
-        this.triggerOnUserChanged();
-        this.triggerOnUserStorageSizeChanged();
+    onUserChanged(user: UserModel | undefined) {
+      this.user = user;
+      this.triggerOnUserChanged();
+      this.triggerOnUserStorageSizeChanged();
     }
 
     registerOnUserStorageSizeChanged(listener: (newUserStorageSize: number) => void):void{
-        this.userStorageSizeListeners.push(listener);
+      this.userStorageSizeListeners.push(listener);
     }
 
     unregisterOnUserStorageSizeChanged(listenerToRemove: (newUserStorageSize: number) => void):void{
-        this.userStorageSizeListeners = this.userStorageSizeListeners.filter(listener => listener !== listenerToRemove);
+      // eslint-disable-next-line max-len
+      this.userStorageSizeListeners = this.userStorageSizeListeners.filter((listener) => listener !== listenerToRemove);
     }
 
     registerOnUserChanged(listener: (newUser: UserModel | undefined) => void):void{
-        this.userListeners.push(listener);
+      this.userListeners.push(listener);
     }
 
     unregisterOnUserChanged(listenerToRemove: (newUser: UserModel | undefined) => void):void{
-        this.userListeners = this.userListeners.filter(listener => listener !== listenerToRemove);
+      this.userListeners = this.userListeners.filter((listener) => listener !== listenerToRemove);
     }
 
     onImageAdded(imageSize: number): void{
-        if(this.user !== undefined){
-            this.user.imageFolderSizeInByte += imageSize;
-            this.triggerOnUserStorageSizeChanged();
-        }
+      if (this.user !== undefined) {
+        this.user.imageFolderSizeInByte += imageSize;
+        this.triggerOnUserStorageSizeChanged();
+      }
     }
 
     onImageRemoved(imageSize: number): void{
-        if(this.user !== undefined){
-            this.user.imageFolderSizeInByte -= imageSize;
-            this.triggerOnUserStorageSizeChanged();
-        }
+      if (this.user !== undefined) {
+        this.user.imageFolderSizeInByte -= imageSize;
+        this.triggerOnUserStorageSizeChanged();
+      }
     }
-    
+
     triggerOnUserStorageSizeChanged(): void {
-        const userImageFolderSize = this.user !== undefined ? this.user.imageFolderSizeInByte : 0;
-        this.userStorageSizeListeners.map(listener => listener(userImageFolderSize));
+      const userImageFolderSize = this.user !== undefined ? this.user.imageFolderSizeInByte : 0;
+      this.userStorageSizeListeners.map((listener) => listener(userImageFolderSize));
     }
 
     triggerOnUserChanged(): void {
-        this.userListeners.map(listener => listener(this.user));
+      this.userListeners.map((listener) => listener(this.user));
     }
 }
 
