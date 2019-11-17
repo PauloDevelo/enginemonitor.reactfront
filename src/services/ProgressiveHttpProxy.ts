66184d23a4c4e14c639b3dc6f3fc59e5e7f535fa
Torruella, Paul <paul.torruella@suez.com>
2019-11-17 09:00:30 +0800

    eslint

--- src/services/ProgressiveHttpProxy.ts
+++ src/services/ProgressiveHttpProxy.ts
@@ -1,16 +1,20 @@
+/* eslint-disable class-methods-use-this */
+
+// eslint-disable-next-line no-unused-vars
+import { CancelToken } from 'axios';
 import syncService from './SyncService';
 import httpProxy from './HttpProxy';
 
+// eslint-disable-next-line no-unused-vars
 import actionManager, { Action, ActionType } from './ActionManager';
 import storageService from './StorageService';
-import { CancelToken } from 'axios';
 
 /**
  * This interface is an enhanced http proxy that manages offline mode.
  */
 export interface ISyncHttpProxy{
     /**
-     * This function execute an http post query and call the function update in the element's "keyname" field returned by the query. 
+     * This function execute an http post query and call the function update in the element's "keyname" field returned by the query.
      * In offline mode, this function will add an action in the history.
      * @param url Post url
      * @param keyName The field name that contains the data to return after calling update on it
@@ -38,47 +42,44 @@ export interface ISyncHttpProxy{
     getArrayOnlineFirst<T>(url: string, keyName:string, init:(model:T) => T, cancelToken?: CancelToken | undefined): Promise<T[]>;
 }
 
-class ProgressiveHttpProxy implements ISyncHttpProxy{
+class ProgressiveHttpProxy implements ISyncHttpProxy {
+  async postAndUpdate<T>(url: string, keyName:string, dataToPost:T, update:(data:T)=>T):Promise<T> {
+    const data:any = { [keyName]: dataToPost };
 
-    async postAndUpdate<T>(url: string, keyName:string, dataToPost:T, update:(data:T)=>T):Promise<T> {
-        const data:any = {[keyName]: dataToPost};
+    if (await syncService.isOnlineAndSynced()) {
+      const savedData = (await httpProxy.post(url, data))[keyName];
 
-        if(await syncService.isOnlineAndSynced()){
-            const savedData = (await httpProxy.post(url, data))[keyName];
-
-            return update(savedData);
-        }
-        else{
-            const action: Action = { key: url, type: ActionType.Post, data: data };
-            await actionManager.addAction(action);
-
-            return dataToPost;
-        }
+      return update(savedData);
     }
 
-    async deleteAndUpdate<T>(url: string, keyName: string, update:(data:T)=>T):Promise<void>{
-        if(await syncService.isOnlineAndSynced()){
-            const deletedEntity = (await httpProxy.deleteReq(url))[keyName];
-            update(deletedEntity);
-        }
-        else{
-            const action: Action = { key: url, type: ActionType.Delete };
-            actionManager.addAction(action);
-        }
+    const action: Action = { key: url, type: ActionType.Post, data };
+    await actionManager.addAction(action);
+
+    return dataToPost;
+  }
+
+  async deleteAndUpdate<T>(url: string, keyName: string, update:(data:T)=>T):Promise<void> {
+    if (await syncService.isOnlineAndSynced()) {
+      const deletedEntity = (await httpProxy.deleteReq(url))[keyName];
+      update(deletedEntity);
+    } else {
+      const action: Action = { key: url, type: ActionType.Delete };
+      actionManager.addAction(action);
+    }
+  }
+
+  async getArrayOnlineFirst<T>(url: string, keyName:string, init:(model:T) => T, cancelToken: CancelToken | undefined = undefined): Promise<T[]> {
+    if (await syncService.isOnlineAndSynced()) {
+      const array = (await httpProxy.get(url, cancelToken))[keyName] as T[];
+      const initArray = array.map(init);
+
+      storageService.setItem<T[]>(url, initArray);
+
+      return initArray;
     }
 
-    async getArrayOnlineFirst<T>(url: string, keyName:string, init:(model:T) => T, cancelToken: CancelToken | undefined = undefined): Promise<T[]> {
-        if(await syncService.isOnlineAndSynced()){
-            const array = (await httpProxy.get(url, cancelToken))[keyName] as T[];
-            const initArray = array.map(init);
-
-            storageService.setItem<T[]>(url, initArray);
-
-            return initArray;
-        }
-        
-        return await storageService.getArray<T>(url);
-    }
+    return storageService.getArray<T>(url);
+  }
 }
 
 const progressiveHttpProxy:ISyncHttpProxy = new ProgressiveHttpProxy();
