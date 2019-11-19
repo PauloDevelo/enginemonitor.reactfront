cd9b871d7e85c8a84a28f97cf92c09f7f349d2dd
Torruella, Paul <paul.torruella@suez.com>
2019-11-20 07:19:35 +0800

    eslint

--- src/components/Alerts/__tests__/Alerts.test.js
+++ src/components/Alerts/__tests__/Alerts.test.js
@@ -1,53 +1,53 @@
-import ignoredMessages from '../../../testHelpers/MockConsole';
-import React, { Fragment } from 'react';
+import React from 'react';
 import { shallow, mount } from 'enzyme';
+import ignoredMessages from '../../../testHelpers/MockConsole';
 
-import Alerts from '../Alerts'
+import Alerts from '../Alerts';
 
-describe('Component Alert', () =>{
-    beforeAll(() => {
-        ignoredMessages.length = 0;
-        ignoredMessages.push("[React Intl] Could not find required `intl` object. <IntlProvider> needs to exist in the component ancestry. Using default message as fallback.");
-    });
+describe('Component Alert', () => {
+  beforeAll(() => {
+    ignoredMessages.length = 0;
+    ignoredMessages.push('[React Intl] Could not find required `intl` object. <IntlProvider> needs to exist in the component ancestry. Using default message as fallback.');
+  });
 
-    it('should render even without props', () => {
-        const wrapper = shallow(<Alerts />);
+  it('should render even without props', () => {
+    const wrapper = shallow(<Alerts />);
 
-        expect(wrapper).toContainExactlyOneMatchingElement("Fragment");
-        expect(wrapper.contains(<Fragment></Fragment>)).toEqual(true);
-    });
+    expect(wrapper).toContainExactlyOneMatchingElement('Fragment');
+    expect(wrapper.contains(<></>)).toEqual(true);
+  });
 
-    it('should render with a message and the default color', () => {
-        const message = "test message";
-        const wrapper = mount(<Alerts error={message}/>);
+  it('should render with a message and the default color', () => {
+    const message = 'test message';
+    const wrapper = mount(<Alerts error={message} />);
 
-        expect(wrapper.text()).toEqual(message);
-        expect(wrapper.find('div').hasClass('alert')).toEqual(true);
-        expect(wrapper.find('div').hasClass('alert-danger')).toEqual(true);
-    });
+    expect(wrapper.text()).toEqual(message);
+    expect(wrapper.find('div').hasClass('alert')).toEqual(true);
+    expect(wrapper.find('div').hasClass('alert-danger')).toEqual(true);
+  });
 
-    it('should render with a message and a secondary color', () => {
-        const message = "test message";
-        const wrapper = mount(<Alerts error={message} color="secondary"/>);
+  it('should render with a message and a secondary color', () => {
+    const message = 'test message';
+    const wrapper = mount(<Alerts error={message} color="secondary" />);
 
-        expect(wrapper.text()).toEqual(message);
-        expect(wrapper.find('div').hasClass('alert')).toEqual(true);
-        expect(wrapper.find('div').hasClass('alert-secondary')).toEqual(true);
-    });
+    expect(wrapper.text()).toEqual(message);
+    expect(wrapper.find('div').hasClass('alert')).toEqual(true);
+    expect(wrapper.find('div').hasClass('alert-secondary')).toEqual(true);
+  });
 
-    it('should render with a composed message and a danger color', () => {
-        const wrapper = mount(<Alerts errors={ {email: 'isrequired'} } />);
+  it('should render with a composed message and a danger color', () => {
+    const wrapper = mount(<Alerts errors={{ email: 'isrequired' }} />);
 
-        expect(wrapper.text()).toEqual('Email is required');
-        expect(wrapper.find('div').hasClass('alert')).toEqual(true);
-        expect(wrapper.find('div').hasClass('alert-danger')).toEqual(true);
-    });
+    expect(wrapper.text()).toEqual('Email is required');
+    expect(wrapper.find('div').hasClass('alert')).toEqual(true);
+    expect(wrapper.find('div').hasClass('alert-danger')).toEqual(true);
+  });
 
-    it('should render with an unexpected composed message and a danger color', () => {
-        const wrapper = mount(<Alerts errors={ {unexpectedField: 'unexpectedMesg'} } />);
+  it('should render with an unexpected composed message and a danger color', () => {
+    const wrapper = mount(<Alerts errors={{ unexpectedField: 'unexpectedMesg' }} />);
 
-        expect(wrapper.text()).toEqual('unexpectedField unexpectedMesg');
-        expect(wrapper.find('div').hasClass('alert')).toEqual(true);
-        expect(wrapper.find('div').hasClass('alert-danger')).toEqual(true);
-    });
+    expect(wrapper.text()).toEqual('unexpectedField unexpectedMesg');
+    expect(wrapper.find('div').hasClass('alert')).toEqual(true);
+    expect(wrapper.find('div').hasClass('alert-danger')).toEqual(true);
+  });
 });
