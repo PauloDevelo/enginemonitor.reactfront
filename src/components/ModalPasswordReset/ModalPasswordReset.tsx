fd0e8111102bbc93f923bd9da3d232f5ab39b8a6
Torruella, Paul <paul.torruella@suez.com>
2019-11-24 07:16:47 +0800

    eslint

--- src/components/ModalPasswordReset/ModalPasswordReset.tsx
+++ src/components/ModalPasswordReset/ModalPasswordReset.tsx
@@ -1,119 +1,122 @@
-import React, { useState, useEffect } from "react";
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Spinner } from 'reactstrap';
-import { faUnlockAlt } from "@fortawesome/free-solid-svg-icons";
+import React, { useState, useEffect } from 'react';
+import {
+  Button, Modal, ModalHeader, ModalBody, ModalFooter, Spinner,
+} from 'reactstrap';
+import { faUnlockAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { CSSTransition } from 'react-transition-group';
 import { FormattedMessage, defineMessages } from 'react-intl';
-import PropTypes from 'prop-types';
 
-import MyForm from "../Form/MyForm"
-import MyInput from "../Form/MyInput"
-import Alerts from "../Alerts/Alerts"
-import ActionButton from "../ActionButton/ActionButton";
+import MyForm from '../Form/MyForm';
+import MyInput from '../Form/MyInput';
+import Alerts from '../Alerts/Alerts';
+import ActionButton from '../ActionButton/ActionButton';
 
 import userProxy from '../../services/UserProxy';
-import HttpError from '../../http/HttpError'
+import HttpError from '../../http/HttpError';
 
 import '../../style/transition.css';
 
-import jsonMessages from "./PasswordReset.messages.json";
+import jsonMessages from './PasswordReset.messages.json';
+
 const changePasswordMsg = defineMessages(jsonMessages);
 
 type NewPassword = {
-	email:string, 
-	newPassword1?:string, 
-	newPassword2?:string
+email:string,
+newPassword1?:string,
+newPassword2?:string
 };
 
 type Props = {
-	visible: boolean, 
-	className: string, 
-	toggle: () => void, 
-	data: NewPassword
+visible: boolean,
+className: string,
+toggle: () => void,
+data: NewPassword
 };
 
 enum ResetPasswordState {
-    Started = 1,
-    IsChanging,
-    InError,
-    Succeed,
+// eslint-disable-next-line no-unused-vars
+Started = 1,
+// eslint-disable-next-line no-unused-vars
+IsChanging,
+// eslint-disable-next-line no-unused-vars
+InError,
+// eslint-disable-next-line no-unused-vars
+Succeed,
 }
 
 type StateType = {
-	state: ResetPasswordState,
-	infoMsg?: string,
-	resetPasswordErrors?: any
+state: ResetPasswordState,
+infoMsg?: string,
+resetPasswordErrors?: any
 }
 
-const ModalPasswordReset = ({visible, className, toggle, data}: Props) => {
-	const [state, setState] = useState<StateType>({state: ResetPasswordState.Started});
+const ModalPasswordReset = ({
+  visible, className, toggle, data,
+}: Props) => {
+  const [state, setState] = useState<StateType>({ state: ResetPasswordState.Started });
 
-	useEffect(() => {
-		setState({state: ResetPasswordState.Started});
-	}, [visible]);
+  useEffect(() => {
+    setState({ state: ResetPasswordState.Started });
+  }, [visible]);
 
-    const cancel = () => {
-		toggle();
-	}
+  const cancel = () => {
+    toggle();
+  };
 
-    const handleSubmit = async(data: NewPassword) => {
-		if(state.state === ResetPasswordState.Succeed){
-			toggle();
-		}
-		else{
-			setState({state: ResetPasswordState.IsChanging});
+  const handleSubmit = async (newData: NewPassword) => {
+    if (state.state === ResetPasswordState.Succeed) {
+      toggle();
+    } else {
+      setState({ state: ResetPasswordState.IsChanging });
 
-			try{
-				if(data.newPassword1 !== undefined && data.newPassword1 === data.newPassword2){
-					await userProxy.resetPassword(data.email, data.newPassword1);
-					setState({state: ResetPasswordState.Succeed, infoMsg: "confirmPasswordChange"});
-				}
-				else{
-					setState({state: ResetPasswordState.InError, resetPasswordErrors: { password: "passwordsHaveToBeIdentical"}});
-				}
-			}
-			catch(errors){
-				if(errors instanceof HttpError){
-					const newResetPasswordErrors = errors.data;
-					setState({state: ResetPasswordState.InError, resetPasswordErrors: newResetPasswordErrors});
-				}
-				else{
-					setState({state: ResetPasswordState.InError});
-				}
-			}
-		}
-	}
-	
-	let submitButtonLabel = state.state === ResetPasswordState.Succeed ? changePasswordMsg.close : changePasswordMsg.changePassword;
+      try {
+        if (newData.newPassword1 !== undefined && newData.newPassword1 === newData.newPassword2) {
+          await userProxy.resetPassword(newData.email, newData.newPassword1);
+          setState({ state: ResetPasswordState.Succeed, infoMsg: 'confirmPasswordChange' });
+        } else {
+          setState({ state: ResetPasswordState.InError, resetPasswordErrors: { password: 'passwordsHaveToBeIdentical' } });
+        }
+      } catch (errors) {
+        if (errors instanceof HttpError) {
+          const newResetPasswordErrors = errors.data;
+          setState({ state: ResetPasswordState.InError, resetPasswordErrors: newResetPasswordErrors });
+        } else {
+          setState({ state: ResetPasswordState.InError });
+        }
+      }
+    }
+  };
 
-	return (
-		<CSSTransition in={visible} timeout={300} classNames="modal">
-			<Modal isOpen={visible} toggle={cancel} className={className} fade={false}>
-				<ModalHeader toggle={cancel}><FontAwesomeIcon icon={faUnlockAlt} />{' '}<FormattedMessage {...changePasswordMsg.modalResetPasswordTitle} /></ModalHeader>
-				<ModalBody>
-				    {visible && <MyForm submit={handleSubmit} id="formChangePassword" initialData={data}>
-                        <MyInput name="email" 			label={changePasswordMsg.email} 			type="email"     	required={true} readonly="true" />
-                        <MyInput name="newPassword1" 	label={changePasswordMsg.newPassword} 		type="password" 	required/>
-						<MyInput name="newPassword2" 	label={changePasswordMsg.retypeNewPassword} type="password" 	required/>
-                    </MyForm>}
-					{state.state === ResetPasswordState.InError && <Alerts errors={ state.resetPasswordErrors } />}
-					{state.state === ResetPasswordState.IsChanging && <Alerts error={"changingPassword"} color="success"><Spinner size="sm" color="secondary" /></Alerts>}
-					{state.state === ResetPasswordState.Succeed && <Alerts error={state.infoMsg} color="success"/>}
-				</ModalBody>
-				<ModalFooter>
-					<ActionButton type="submit" form="formChangePassword" color="success" message={submitButtonLabel} isActing={state.state === ResetPasswordState.IsChanging} />
-                    {state.state !== ResetPasswordState.Succeed && <Button color="secondary" onClick={cancel}><FormattedMessage {...changePasswordMsg.cancel} /></Button>}
-				</ModalFooter>
-			</Modal>
-		</CSSTransition>
-	);
-}
+  const submitButtonLabel = state.state === ResetPasswordState.Succeed ? changePasswordMsg.close : changePasswordMsg.changePassword;
 
-ModalPasswordReset.propTypes = {
-    toggle: PropTypes.func.isRequired,
-	visible: PropTypes.bool.isRequired,
-	className: PropTypes.string,
-	data: PropTypes.object
+  return (
+    <CSSTransition in={visible} timeout={300} classNames="modal">
+      <Modal isOpen={visible} toggle={cancel} className={className} fade={false}>
+        <ModalHeader toggle={cancel}>
+          <FontAwesomeIcon icon={faUnlockAlt} />
+          {' '}
+          <FormattedMessage {...changePasswordMsg.modalResetPasswordTitle} />
+        </ModalHeader>
+        <ModalBody>
+          {visible && (
+          <MyForm submit={handleSubmit} id="formChangePassword" initialData={data}>
+            <MyInput name="email" label={changePasswordMsg.email} type="email" required readonly="true" />
+            <MyInput name="newPassword1" label={changePasswordMsg.newPassword} type="password" required />
+            <MyInput name="newPassword2" label={changePasswordMsg.retypeNewPassword} type="password" required />
+          </MyForm>
+          )}
+          {state.state === ResetPasswordState.InError && <Alerts errors={state.resetPasswordErrors} />}
+          {state.state === ResetPasswordState.IsChanging && <Alerts error="changingPassword" color="success"><Spinner size="sm" color="secondary" /></Alerts>}
+          {state.state === ResetPasswordState.Succeed && <Alerts error={state.infoMsg} color="success" />}
+        </ModalBody>
+        <ModalFooter>
+          <ActionButton type="submit" form="formChangePassword" color="success" message={submitButtonLabel} isActing={state.state === ResetPasswordState.IsChanging} />
+          {state.state !== ResetPasswordState.Succeed && <Button color="secondary" onClick={cancel}><FormattedMessage {...changePasswordMsg.cancel} /></Button>}
+        </ModalFooter>
+      </Modal>
+    </CSSTransition>
+  );
 };
 
 export default ModalPasswordReset;
