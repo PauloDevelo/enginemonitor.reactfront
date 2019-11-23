33f09d687385f7f0e0444485296018b35e9d7cc2
Torruella, Paul <paul.torruella@suez.com>
2019-11-23 22:48:25 +0800

    MyInput eslint

--- src/components/Form/MyInput.tsx
+++ src/components/Form/MyInput.tsx
@@ -1,89 +1,102 @@
-import React, { useState, useRef, useEffect, useMemo, useCallback } from 'react';
-import { FormGroup, Label, Input, FormFeedback } from 'reactstrap';
+import React, {
+  useState, useRef, useEffect, useMemo, useCallback,
+} from 'react';
+import {
+  FormGroup, Label, Input, FormFeedback,
+} from 'reactstrap';
+// eslint-disable-next-line no-unused-vars
+import { FormattedMessage, MessageDescriptor } from 'react-intl';
 import ToolTip from '../ToolTip/ToolTip';
 
-import { FormattedMessage, MessageDescriptor } from 'react-intl';
-
 type Props = {
-	label: MessageDescriptor,
-	tooltip?: MessageDescriptor,
-	handleChange?: (name: string, value: string | boolean) => void,
-	onChanged?: (newValue: any) => void,
-	validationTrigger?: number,
-	type: any,
-	name: string,
-	min?: number,
-	max?: number,
-	required?: boolean,
-	readonly?: string,
-	children?: JSX.Element[] | JSX.Element
+label: MessageDescriptor,
+tooltip?: MessageDescriptor,
+handleChange?: (name: string, value: string | boolean) => void,
+onChanged?: (newValue: any) => void,
+validationTrigger?: number,
+type: any,
+name: string,
+min?: number,
+max?: number,
+required?: boolean,
+readonly?: string,
+children?: JSX.Element[] | JSX.Element
 };
 
-const MyInput = React.memo(function MyInput({ label, tooltip, handleChange, onChanged, validationTrigger, children, ...props }:Props){
-	const [validity, setValidity] = useState({ isValid: true, errorMessage: '' });
-	const inputElemRef = useRef<any>();
-	const tooltipElement = useMemo(() => { return tooltip ? ToolTip({tooltip}) : undefined }, [tooltip]);
+const MyInput = React.memo(({
+  label, tooltip, handleChange, onChanged, validationTrigger, children, ...props
+}:Props) => {
+  const [validity, setValidity] = useState({ isValid: true, errorMessage: '' });
+  const inputElemRef = useRef<any>();
+  const tooltipElement = useMemo(() => (tooltip ? ToolTip({ tooltip }) : undefined), [tooltip]);
 
-	const validate = () => {
-		const inputElem = inputElemRef.current;
-		if(inputElem !== undefined){
-			setValidity( { 
-				isValid: inputElem.validity !== undefined ? inputElem.validity.valid : true, 
-				errorMessage: inputElem.validationMessage 
-			});
-		}
-	};
+  const validate = () => {
+    const inputElem = inputElemRef.current;
+    if (inputElem !== undefined) {
+      setValidity({
+        isValid: inputElem.validity !== undefined ? inputElem.validity.valid : true,
+        errorMessage: inputElem.validationMessage,
+      });
+    }
+  };
 
-	useEffect(() => {
-		if(validationTrigger !== undefined && validationTrigger !== 0){
-			validate();
-		}
-	}, [validationTrigger]);
+  useEffect(() => {
+    if (validationTrigger !== undefined && validationTrigger !== 0) {
+      validate();
+    }
+  }, [validationTrigger]);
 
-	useEffect(() => {
-		setValidity({ isValid: true, errorMessage: '' });
-	}, [inputElemRef]);
-	
-	const onChangeHandler = useCallback((event:React.ChangeEvent<HTMLInputElement>):void => {
-		validate();
+  useEffect(() => {
+    setValidity({ isValid: true, errorMessage: '' });
+  }, [inputElemRef]);
 
-		const target:HTMLInputElement = event.target as HTMLInputElement;
-		const newValue = target.type === 'checkbox' ? target.checked : target.value;
+  const onChangeHandler = useCallback((event:React.ChangeEvent<HTMLInputElement>):void => {
+    validate();
 
-		if(typeof handleChange === 'function'){
-			handleChange(props.name, newValue);
-		}
+    const target:HTMLInputElement = event.target as HTMLInputElement;
+    const newValue = target.type === 'checkbox' ? target.checked : target.value;
 
-		if(typeof onChanged === 'function'){
-			onChanged(newValue);
-		}
-	}, [handleChange, onChanged, props.name]);
-	
-	if (props.type === 'checkbox'){
-		const inlineValue = "true" as never;
-		return (
-			<FormGroup className={"form-group"} check inline={true}>
-				<Label check inline={inlineValue}>
-					<FormattedMessage {...label}/>{tooltipElement}{' '}
-					<Input ref={inputElemRef} onChange={onChangeHandler} invalid={!validity.isValid} {...props}/>
-				</Label>
-			</FormGroup>
-		);
-	}
+    if (typeof handleChange === 'function') {
+      handleChange(props.name, newValue);
+    }
 
-	return ( 
-		<FormGroup className={"form-group"}>
-			<Label for={props.name}><FormattedMessage {...label} />{tooltipElement}</Label>
-			<Input 	innerRef={inputElemRef} 
-					className={"form-control"}
-					onChange={onChangeHandler} 
-					invalid={!validity.isValid}
-					{...props}>
-				{children}
-			</Input>
-			{!validity.isValid && <FormFeedback>{validity.errorMessage}</FormFeedback>}
-		</FormGroup>
-	)
+    if (typeof onChanged === 'function') {
+      onChanged(newValue);
+    }
+  }, [handleChange, onChanged, props.name]);
+
+  if (props.type === 'checkbox') {
+    const inlineValue = 'true' as never;
+    return (
+      <FormGroup className="form-group" check inline>
+        <Label check inline={inlineValue}>
+          <FormattedMessage {...label} />
+          {tooltipElement}
+          {' '}
+          <Input ref={inputElemRef} onChange={onChangeHandler} invalid={!validity.isValid} {...props} />
+        </Label>
+      </FormGroup>
+    );
+  }
+
+  return (
+    <FormGroup className="form-group">
+      <Label for={props.name}>
+        <FormattedMessage {...label} />
+        {tooltipElement}
+      </Label>
+      <Input
+        innerRef={inputElemRef}
+        className="form-control"
+        onChange={onChangeHandler}
+        invalid={!validity.isValid}
+        {...props}
+      >
+        {children}
+      </Input>
+      {!validity.isValid && <FormFeedback>{validity.errorMessage}</FormFeedback>}
+    </FormGroup>
+  );
 });
 
 export default MyInput;
