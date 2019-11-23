892e3429367fb0997e28d71b2a6337f34b4e47ff
Torruella, Paul <paul.torruella@suez.com>
2019-11-23 23:09:27 +0800

    add a unit test to test the authentication

--- src/components/ModalLogin/__tests__/ModalLogin.test.js
+++ src/components/ModalLogin/__tests__/ModalLogin.test.js
@@ -26,22 +26,63 @@ describe('ModalLogin', () => {
     userProxy.authenticate.mockReset();
   });
 
+  const user = {
+    _uiId: 'user_01',
+    name: 'torruella',
+    email: 'paulodevelo@lovestreet.com',
+    password: 'mypassword',
+    firstname: 'paul',
+    imageFolderSizeInByte: 1234,
+    imageFolderSizeLimitInByte: 10000,
+  };
+
 
   it('should render the ModalLogin', async () => {
     // Arrange
-    let isVisible = true;
+    let signupVisible = false;
     const toggleModalSignup = jest.fn().mockImplementation(() => {
-      isVisible = !isVisible;
+      signupVisible = !signupVisible;
     });
     const onLoggedIn = jest.fn();
 
 
     // Act
-    const modalLogin = mount(<ModalLogin visible={isVisible} onLoggedIn={onLoggedIn} className="modal-dialog-centered" toggleModalSignup={toggleModalSignup} />);
+    const modalLogin = mount(<ModalLogin visible onLoggedIn={onLoggedIn} className="modal-dialog-centered" toggleModalSignup={toggleModalSignup} />);
     await updateWrapper(modalLogin);
 
     // Assert
     expect(modalLogin).toMatchSnapshot();
     expect(modalLogin.props().visible).toBe(true);
+  });
+
+  it('should authenticate with the value input', async () => {
+    // Arrange
+    let signupVisible = false;
+    const toggleModalSignup = jest.fn().mockImplementation(() => {
+      signupVisible = !signupVisible;
+    });
+    const onLoggedIn = jest.fn();
+
+    jest.spyOn(userProxy, 'authenticate').mockImplementation(() => Promise.resolve(user));
+
+
+    const modalLogin = mount(<ModalLogin visible onLoggedIn={onLoggedIn} className="modal-dialog-centered" toggleModalSignup={toggleModalSignup} />);
+    await updateWrapper(modalLogin);
+
+    // Act
+    const myForm = modalLogin.find('Memo(MyForm)');
+    const inputs = myForm.find('input');
+    inputs.at(0).simulate('change', { target: { value: 'paulodevelo@lovestreet.com' } });
+    inputs.at(1).simulate('change', { target: { value: 'mypassword' } });
+    inputs.at(2).simulate('change', { target: { type: 'checkbox', checked: true } });
+    myForm.simulate('submit');
+    await updateWrapper(modalLogin);
+
+    // Assert
+    expect(userProxy.authenticate).toHaveBeenCalledTimes(1);
+    expect(userProxy.authenticate.mock.calls[0][0]).toEqual({ email: 'paulodevelo@lovestreet.com', password: 'mypassword', remember: true });
+
+    expect(onLoggedIn).toBeCalledTimes(1);
+    expect(onLoggedIn.mock.calls[0][0]).toEqual(user);
   });
 });
