b900a0a696a4f58a7e481bbe8611ef54d23c7ce9
Torruella, Paul <paul.torruella@suez.com>
2019-11-24 08:59:34 +0800

    First UT for ModalPassword with Snapshot

---
+++ src/components/ModalPasswordReset/__tests__/ModalPasswordReset.test.js
@@ -0,0 +1,137 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+// eslint-disable-next-line no-unused-vars
+import localforage from 'localforage';
+
+import ignoredMessages from '../../../testHelpers/MockConsole';
+
+import userProxy from '../../../services/UserProxy';
+
+import ModalPasswordReset from '../ModalPasswordReset';
+import updateWrapper from '../../../testHelpers/EnzymeHelper';
+import HttpError from '../../../http/HttpError';
+
+jest.mock('../../../services/UserProxy');
+jest.mock('localforage');
+
+describe('ModalPasswordReset', () => {
+  beforeAll(() => {
+    ignoredMessages.length = 0;
+    ignoredMessages.push('[React Intl] Could not find required `intl` object. <IntlProvider> needs to exist in the component ancestry. Using default message as fallback.');
+    ignoredMessages.push('a test was not wrapped in act');
+  });
+
+  afterEach(() => {
+    userProxy.resetPassword.mockReset();
+  });
+
+  const data = { email: 'paulodevelo@lovestreet.com', newPassword1: '', newPassword2: '' };
+
+  it('should render the ModalPasswordReset', async () => {
+    // Arrange
+    const toggle = jest.fn();
+
+    // Act
+    const modalPasswordReset = mount(<ModalPasswordReset visible toggle={toggle} data={data} />);
+    await updateWrapper(modalPasswordReset);
+
+    // Assert
+    expect(modalPasswordReset).toMatchSnapshot();
+    expect(modalPasswordReset.find('ModalFooter').find('Button').length).toBe(2);
+
+    const alerts = modalPasswordReset.find('Alerts');
+    expect(alerts.length).toBe(0);
+  });
+
+  // it('should close the modal if the user click Cancel', async () => {
+  //   // Arrange
+  //   uuidv1.mockImplementation(() => 'user_01');
+
+  //   const toggle = jest.fn();
+
+  //   const modalSignup = mount(<ModalSignup visible toggle={toggle} />);
+  //   await updateWrapper(modalSignup);
+
+  //   const cancelButton = modalSignup.find('ModalFooter').find('Button').at(1);
+
+  //   // Act
+  //   cancelButton.simulate('click');
+  //   await updateWrapper(modalSignup);
+
+  //   // Assert
+  //   expect(modalSignup.find('ModalFooter').find('Button').length).toBe(2);
+
+  //   const alerts = modalSignup.find('Alerts');
+  //   expect(alerts.length).toBe(0);
+  //   expect(toggle).toBeCalledTimes(1);
+  // });
+
+  // it('should call signup with all the data input when the user click on signup', async () => {
+  //   // Arrange
+  //   uuidv1.mockImplementation(() => user._uiId);
+
+  //   const toggle = jest.fn();
+  //   jest.spyOn(userProxy, 'signup').mockImplementation(() => Promise.resolve());
+
+  //   const modalSignup = mount(<ModalSignup visible toggle={toggle} />);
+  //   await updateWrapper(modalSignup);
+
+  //   const myForm = modalSignup.find('Memo(MyForm)');
+  //   const inputs = myForm.find('input');
+  //   inputs.at(0).simulate('change', { target: { value: user.name } });
+  //   inputs.at(1).simulate('change', { target: { value: user.firstname } });
+  //   inputs.at(2).simulate('change', { target: { value: user.email } });
+  //   inputs.at(3).simulate('change', { target: { value: user.password } });
+  //   await updateWrapper(modalSignup);
+
+  //   // Act
+  //   modalSignup.find('Memo(MyForm)').simulate('submit');
+  //   await updateWrapper(modalSignup);
+
+  //   // Assert
+  //   expect(modalSignup.find('ModalFooter').find('Button').length).toBe(2);
+
+  //   const alerts = modalSignup.find('Alerts');
+  //   expect(alerts.length).toBe(1);
+  //   expect(alerts.props()).toEqual({ error: 'emailSent', color: 'success' });
+  //   expect(toggle).toBeCalledTimes(0);
+
+  //   expect(userProxy.signup).toBeCalledTimes(1);
+  //   expect(userProxy.signup.mock.calls[0][0]).toEqual(user);
+  // });
+
+  // it('should display an error if an error occurs during signup', async () => {
+  //   // Arrange
+  //   uuidv1.mockImplementation(() => user._uiId);
+
+  //   const toggle = jest.fn();
+  //   jest.spyOn(userProxy, 'signup').mockImplementation(() => Promise.reject(new HttpError({ email: 'alreadyexisting' })));
+
+  //   const modalSignup = mount(<ModalSignup visible toggle={toggle} />);
+  //   await updateWrapper(modalSignup);
+
+  //   const myForm = modalSignup.find('Memo(MyForm)');
+  //   const inputs = myForm.find('input');
+  //   inputs.at(0).simulate('change', { target: { value: user.name } });
+  //   inputs.at(1).simulate('change', { target: { value: user.firstname } });
+  //   inputs.at(2).simulate('change', { target: { value: user.email } });
+  //   inputs.at(3).simulate('change', { target: { value: user.password } });
+  //   await updateWrapper(modalSignup);
+
+  //   // Act
+  //   modalSignup.find('Memo(MyForm)').simulate('submit');
+  //   await updateWrapper(modalSignup);
+
+  //   // Assert
+  //   expect(modalSignup.find('ModalFooter').find('Button').length).toBe(2);
+
+  //   const alerts = modalSignup.find('Alerts');
+  //   expect(alerts.length).toBe(1);
+  //   expect(alerts.props()).toEqual({ errors: { email: 'alreadyexisting' } });
+  //   expect(toggle).toBeCalledTimes(0);
+
+  //   expect(userProxy.signup).toBeCalledTimes(1);
+  //   expect(userProxy.signup.mock.calls[0][0]).toEqual(user);
+  // });
+});
