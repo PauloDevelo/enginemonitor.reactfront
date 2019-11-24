f6bddf7a2e25185f96045c49f76555a989cd17a3
Torruella, Paul <paul.torruella@suez.com>
2019-11-24 19:34:07 +0800

    Add UTs for the ImageFolderGauge component

---
+++ src/components/NavBar/__tests__/ImageFolderGauge.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+// eslint-disable-next-line no-unused-vars
+import localforage from 'localforage';
+
+import ImageFolderGauge from '../ImageFolderGauge';
+
+import ignoredMessages from '../../../testHelpers/MockConsole';
+
+jest.mock('localforage');
+
+describe('ImageFolderGauge', () => {
+  beforeEach(() => {
+  });
+
+  beforeAll(() => {
+    ignoredMessages.length = 0;
+    ignoredMessages.push('Could not find required `intl` object.');
+  });
+
+  afterEach(async () => {
+  });
+
+  const imageFolderStatus = [
+    { storageSizeInMB: 35, storageSizeLimitInMB: 100 },
+    { storageSizeInMB: 60, storageSizeLimitInMB: 100 },
+    { storageSizeInMB: 85, storageSizeLimitInMB: 100 },
+  ];
+
+  describe.each(imageFolderStatus)('Render', ({ storageSizeInMB, storageSizeLimitInMB }) => {
+    it(`should display the image gauge when ${storageSizeInMB} Mb are used over ${storageSizeLimitInMB}`, async (done) => {
+      // Arrange
+
+      // Act
+      const imageFolderGaugeWrapper = mount(<ImageFolderGauge storageSizeInMB={storageSizeInMB} storageSizeLimitInMB={storageSizeLimitInMB} />);
+
+      // Assert
+      expect(imageFolderGaugeWrapper).toMatchSnapshot();
+      done();
+    });
+  });
+});
