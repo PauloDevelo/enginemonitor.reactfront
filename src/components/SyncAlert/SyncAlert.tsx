31287bce9e8af338e8cc9aa191335fee3d0cf9f6
Torruella, Paul <paul.torruella@suez.com>
2019-11-24 09:38:30 +0800

    eslint

--- src/components/SyncAlert/SyncAlert.tsx
+++ src/components/SyncAlert/SyncAlert.tsx
@@ -1,41 +1,49 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import {UncontrolledAlert, Progress} from 'reactstrap';
+import { UncontrolledAlert, Progress } from 'reactstrap';
 
 import { FormattedMessage, defineMessages } from 'react-intl';
 
-import syncService, {SyncContext} from '../../services/SyncService';
+// eslint-disable-next-line no-unused-vars
+import syncService, { SyncContext } from '../../services/SyncService';
 
-import jsonMessages from "./SyncAlert.messages.json";
+import jsonMessages from './SyncAlert.messages.json';
+
 const syncAlertMsg = defineMessages(jsonMessages);
 
 type Type = {
     className: string
 }
 
-const SyncAlert = ({className}:Type) => {
-    const initialContext:SyncContext = {
-        isSyncing : false,
-        totalActionToSync : 0,
-        remainingActionToSync : 0
-    };
-    const [syncContext, setSyncContext] = useState(initialContext);
+const SyncAlert = ({ className }:Type) => {
+  const initialContext:SyncContext = {
+    isSyncing: false,
+    totalActionToSync: 0,
+    remainingActionToSync: 0,
+  };
+  const [syncContext, setSyncContext] = useState(initialContext);
 
-    useEffect(() => {
-        syncService.registerSyncListener(setSyncContext);
-        
-        return () => syncService.unregisterSyncListener(setSyncContext);
-    }, []);
+  useEffect(() => {
+    syncService.registerSyncListener(setSyncContext);
+
+    return () => syncService.unregisterSyncListener(setSyncContext);
+  }, []);
 
 
-    return (
-        <Fragment>
-            {syncContext.isSyncing && <UncontrolledAlert color="warning" className={className}>
-                <div className="text-center"><FormattedMessage {...syncAlertMsg.syncInProgress} /></div>
-                <Progress animated color="warning" value={syncContext.remainingActionToSync * 100 / syncContext.totalActionToSync} >{syncContext.remainingActionToSync}/{syncContext.totalActionToSync}</Progress>
-            </UncontrolledAlert>}
-        </Fragment>
-    );
-}
+  return (
+    <>
+      {syncContext.isSyncing && (
+        <UncontrolledAlert color="warning" className={className}>
+          <div className="text-center"><FormattedMessage {...syncAlertMsg.syncInProgress} /></div>
+          <Progress animated color="warning" value={syncContext.remainingActionToSync * 100 / syncContext.totalActionToSync}>
+            {syncContext.remainingActionToSync}
+/
+            {syncContext.totalActionToSync}
+          </Progress>
+        </UncontrolledAlert>
+      )}
+    </>
+  );
+};
 
 export default SyncAlert;
