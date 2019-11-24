c798ed3d1b9a82ccbee874f982b21f340ca59218
Torruella, Paul <paul.torruella@suez.com>
2019-11-24 19:24:28 +0800

    eslint + simplification

--- src/components/NavBar/ImageFolderGauge.tsx
+++ src/components/NavBar/ImageFolderGauge.tsx
@@ -1,45 +1,43 @@
-import React, { useState, useEffect } from "react";
-import { Progress } from "reactstrap";
+import React, { useState, useEffect } from 'react';
+import { Progress } from 'reactstrap';
 
 type Props = {
     storageSizeInMB: number,
     storageSizeLimitInMB: number
 };
 
-const getColorProgress = (size: number, sizeLimit: number):string  => {
-    const ratio = size / sizeLimit;
+const getColorProgress = (size: number, sizeLimit: number):string => {
+  const ratio = size / sizeLimit;
 
-    if (ratio < 0.5){
-        return "success";
-    }
-    else if (ratio >=0.5 && ratio < 0.75){
-        return "warning";
-    }
-    else{
-        return "danger";
-    }
-}
+  if (ratio < 0.5) {
+    return 'success';
+  }
+  if (ratio >= 0.5 && ratio < 0.75) {
+    return 'warning';
+  }
 
-const ImageFolderGauge = ({storageSizeInMB, storageSizeLimitInMB}:Props) => {
-    const [color, setColor] = useState(getColorProgress(storageSizeInMB, storageSizeLimitInMB));
+  return 'danger';
+};
 
-    useEffect(() => {
-        setColor(getColorProgress(storageSizeInMB, storageSizeLimitInMB));
-    }, [storageSizeInMB, storageSizeLimitInMB]);
+const formatMbInCurrentCulture = (sizeInMb: number):string => new Intl.NumberFormat(window.navigator.languages[0], { maximumSignificantDigits: 2 }).format(sizeInMb);
 
-    const getStorageSizeInMBFormated = () => {
-        return formatMbInCurrentCulture(storageSizeInMB);
-    }
+const ImageFolderGauge = ({ storageSizeInMB, storageSizeLimitInMB }:Props) => {
+  const [color, setColor] = useState(getColorProgress(storageSizeInMB, storageSizeLimitInMB));
 
-    const getStorageSizeLimitInMBFormated = () => {
-        return formatMbInCurrentCulture(storageSizeLimitInMB);
-    }
+  useEffect(() => {
+    setColor(getColorProgress(storageSizeInMB, storageSizeLimitInMB));
+  }, [storageSizeInMB, storageSizeLimitInMB]);
 
-    const formatMbInCurrentCulture = (sizeInMb: number):string => {
-        return new Intl.NumberFormat(window.navigator.languages[0], { maximumSignificantDigits: 2 }).format(sizeInMb);
-    }
-
-    return <Progress color={color} value={storageSizeInMB} max={storageSizeLimitInMB}>{getStorageSizeInMBFormated()} of {getStorageSizeLimitInMBFormated()}MB</Progress>
-}
+  return (
+    <Progress color={color} value={storageSizeInMB} max={storageSizeLimitInMB}>
+      {formatMbInCurrentCulture(storageSizeInMB)}
+      {' '}
+of
+      {' '}
+      {formatMbInCurrentCulture(storageSizeLimitInMB)}
+MB
+    </Progress>
+  );
+};
 
 export default React.memo(ImageFolderGauge);
