279ec104af3184cc23128f9b754556e1bf3da372
Torruella, Paul <paul.torruella@suez.com>
2019-11-24 18:12:51 +0800

    Add UT for ConnectionStateMessage

---
+++ src/components/NavBar/__tests__/ConnectionStateMessage.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+// eslint-disable-next-line no-unused-vars
+import localforage from 'localforage';
+
+import ConnectionStateMessage from '../ConnectionStateMessage';
+
+import ignoredMessages from '../../../testHelpers/MockConsole';
+import updateWrapper from '../../../testHelpers/EnzymeHelper';
+
+jest.mock('localforage');
+
+describe('ConnectionStateMessage', () => {
+  beforeEach(() => {
+  });
+
+  beforeAll(() => {
+    ignoredMessages.length = 0;
+    ignoredMessages.push('Could not find required `intl` object.');
+  });
+
+  afterEach(async () => {
+  });
+
+  const connectionStates = [
+    { isOnline: true, isSynced: true },
+    { isOnline: true, isSynced: false },
+    { isOnline: false, isSynced: true },
+    { isOnline: false, isSynced: false },
+  ];
+
+  describe.each(connectionStates)('synchronize', ({ isOnline, isSynced }) => {
+    it(`should display the connection when isOnline is ${isOnline} and the application is synced ${isSynced}`, async (done) => {
+      // Arrange
+
+      // Act
+      const connectionStateMessageWrapper = mount(<ConnectionStateMessage isOnline={isOnline} isSynced={isSynced} />);
+
+      // Assert
+      expect(connectionStateMessageWrapper).toMatchSnapshot();
+      done();
+    });
+  });
+});
