94ac64862f2a780bfb4f1ed1b5b362c8515acc68
Torruella, Paul <paul.torruella@suez.com>
2019-11-24 08:46:37 +0800

    UT for  ModalSignup

--- src/components/ModalSignup/__tests__/ModalSignup.test.js
+++ src/components/ModalSignup/__tests__/ModalSignup.test.js
@@ -34,18 +34,15 @@ describe('ModalSignup', () => {
     email: 'paulodevelo@lovestreet.com',
     password: 'mypassword',
     firstname: 'paul',
-    imageFolderSizeInByte: 1234,
-    imageFolderSizeLimitInByte: 10000,
+    imageFolderSizeInByte: 0,
+    imageFolderSizeLimitInByte: 0,
   };
 
   it('should render the ModalSignup', async () => {
     // Arrange
     uuidv1.mockImplementation(() => 'user_01');
 
-    let signupVisible = false;
-    const toggle = jest.fn().mockImplementation(() => {
-      signupVisible = !signupVisible;
-    });
+    const toggle = jest.fn();
 
     // Act
     const modalSignup = mount(<ModalSignup visible toggle={toggle} />);
@@ -57,5 +54,96 @@ describe('ModalSignup', () => {
 
     const alerts = modalSignup.find('Alerts');
     expect(alerts.length).toBe(0);
+  });
+
+  it('should close the modal if the user click Cancel', async () => {
+    // Arrange
+    uuidv1.mockImplementation(() => 'user_01');
+
+    const toggle = jest.fn();
+
+    const modalSignup = mount(<ModalSignup visible toggle={toggle} />);
+    await updateWrapper(modalSignup);
+
+    const cancelButton = modalSignup.find('ModalFooter').find('Button').at(1);
+
+    // Act
+    cancelButton.simulate('click');
+    await updateWrapper(modalSignup);
+
+    // Assert
+    expect(modalSignup.find('ModalFooter').find('Button').length).toBe(2);
+
+    const alerts = modalSignup.find('Alerts');
+    expect(alerts.length).toBe(0);
+    expect(toggle).toBeCalledTimes(1);
+  });
+
+  it('should call signup with all the data input when the user click on signup', async () => {
+    // Arrange
+    uuidv1.mockImplementation(() => user._uiId);
+
+    const toggle = jest.fn();
+    jest.spyOn(userProxy, 'signup').mockImplementation(() => Promise.resolve());
+
+    const modalSignup = mount(<ModalSignup visible toggle={toggle} />);
+    await updateWrapper(modalSignup);
+
+    const myForm = modalSignup.find('Memo(MyForm)');
+    const inputs = myForm.find('input');
+    inputs.at(0).simulate('change', { target: { value: user.name } });
+    inputs.at(1).simulate('change', { target: { value: user.firstname } });
+    inputs.at(2).simulate('change', { target: { value: user.email } });
+    inputs.at(3).simulate('change', { target: { value: user.password } });
+    await updateWrapper(modalSignup);
+
+    // Act
+    modalSignup.find('Memo(MyForm)').simulate('submit');
+    await updateWrapper(modalSignup);
+
+    // Assert
+    expect(modalSignup.find('ModalFooter').find('Button').length).toBe(2);
+
+    const alerts = modalSignup.find('Alerts');
+    expect(alerts.length).toBe(1);
+    expect(alerts.props()).toEqual({ error: 'emailSent', color: 'success' });
+    expect(toggle).toBeCalledTimes(0);
+
+    expect(userProxy.signup).toBeCalledTimes(1);
+    expect(userProxy.signup.mock.calls[0][0]).toEqual(user);
+  });
+
+  it('should display an error if an error occurs during signup', async () => {
+    // Arrange
+    uuidv1.mockImplementation(() => user._uiId);
+
+    const toggle = jest.fn();
+    jest.spyOn(userProxy, 'signup').mockImplementation(() => Promise.reject(new HttpError({ email: 'alreadyexisting' })));
+
+    const modalSignup = mount(<ModalSignup visible toggle={toggle} />);
+    await updateWrapper(modalSignup);
+
+    const myForm = modalSignup.find('Memo(MyForm)');
+    const inputs = myForm.find('input');
+    inputs.at(0).simulate('change', { target: { value: user.name } });
+    inputs.at(1).simulate('change', { target: { value: user.firstname } });
+    inputs.at(2).simulate('change', { target: { value: user.email } });
+    inputs.at(3).simulate('change', { target: { value: user.password } });
+    await updateWrapper(modalSignup);
+
+    // Act
+    modalSignup.find('Memo(MyForm)').simulate('submit');
+    await updateWrapper(modalSignup);
+
+    // Assert
+    expect(modalSignup.find('ModalFooter').find('Button').length).toBe(2);
+
+    const alerts = modalSignup.find('Alerts');
+    expect(alerts.length).toBe(1);
+    expect(alerts.props()).toEqual({ errors: { email: 'alreadyexisting' } });
+    expect(toggle).toBeCalledTimes(0);
+
+    expect(userProxy.signup).toBeCalledTimes(1);
+    expect(userProxy.signup.mock.calls[0][0]).toEqual(user);
   });
 });
