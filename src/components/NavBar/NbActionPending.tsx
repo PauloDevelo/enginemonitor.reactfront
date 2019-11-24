68559d5f79a5cad7d9bae4466ed3e08eede7b6e4
Torruella, Paul <paul.torruella@suez.com>
2019-11-24 19:20:42 +0800

    eslint

--- src/components/NavBar/NbActionPending.tsx
+++ src/components/NavBar/NbActionPending.tsx
@@ -1,26 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 import { FormattedMessage, defineMessages } from 'react-intl';
 
 import actionManager from '../../services/ActionManager';
 
-import jsonMessages from "./NavBar.messages.json";
+import jsonMessages from './NavBar.messages.json';
+
 const navBarMsg = defineMessages(jsonMessages);
 
 
 const NbActionPending = () => {
-    const [nbAction, setActionCounter] = useState(0);
+  const [nbAction, setActionCounter] = useState(0);
 
-    useEffect(() => {
-        actionManager.registerOnActionManagerChanged(setActionCounter);
-        actionManager.countAction().then(setActionCounter);
+  useEffect(() => {
+    actionManager.registerOnActionManagerChanged(setActionCounter);
+    actionManager.countAction().then(setActionCounter);
 
-        return () => {
-            actionManager.unregisterOnActionManagerChanged(setActionCounter);
-        }
-    }, []);
+    return () => {
+      actionManager.unregisterOnActionManagerChanged(setActionCounter);
+    };
+  }, []);
 
-    return <FormattedMessage {... navBarMsg.nbAction} values={{nbAction}}/>
-}
+  return <FormattedMessage {... navBarMsg.nbAction} values={{ nbAction }} />;
+};
 
 export default NbActionPending;
