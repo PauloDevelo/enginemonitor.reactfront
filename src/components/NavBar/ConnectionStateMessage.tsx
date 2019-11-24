a9e79713ed2a7978b5ad8443b13ef91dc18699b1
Torruella, Paul <paul.torruella@suez.com>
2019-11-24 17:54:22 +0800

    eslint

--- src/components/NavBar/ConnectionStateMessage.tsx
+++ src/components/NavBar/ConnectionStateMessage.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React from 'react';
 
 import { FormattedMessage, defineMessages } from 'react-intl';
 
-import jsonMessages from "./NavBar.messages.json";
+import jsonMessages from './NavBar.messages.json';
+
 const navBarMsg = defineMessages(jsonMessages);
 
 type ConnectionState = {
@@ -10,23 +11,20 @@ type ConnectionState = {
     isSynced: boolean
 };
 
-const ConnectionStateMessage = ({isOnline, isSynced}:ConnectionState) => {
-    if(isSynced){
-        if (isOnline){
-            return <FormattedMessage {...navBarMsg.online} />
-        }
-        else{
-            return <FormattedMessage {...navBarMsg.notonline} />
-        }
+const ConnectionStateMessage = ({ isOnline, isSynced }:ConnectionState) => {
+  if (isSynced) {
+    if (isOnline) {
+      return <FormattedMessage {...navBarMsg.online} />;
     }
-    else{
-        if (isOnline){
-            return <FormattedMessage {...navBarMsg.syncAction} />
-        }
-        else{
-            return <FormattedMessage {...navBarMsg.notonline} />
-        }
-    }
-}
+
+    return <FormattedMessage {...navBarMsg.notonline} />;
+  }
+
+  if (isOnline) {
+    return <FormattedMessage {...navBarMsg.syncAction} />;
+  }
+
+  return <FormattedMessage {...navBarMsg.notonline} />;
+};
 
 export default React.memo(ConnectionStateMessage);
