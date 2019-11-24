54e7d1d7108f0c70e4ef54122efebc47dd91b417
Torruella, Paul <paul.torruella@suez.com>
2019-11-24 18:56:01 +0800

    eslint

--- src/components/NavBar/DropDownConnectionStateItem.tsx
+++ src/components/NavBar/DropDownConnectionStateItem.tsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
-import {  DropdownItem } from 'reactstrap';
+import React, { useState, useEffect } from 'react';
+import { DropdownItem } from 'reactstrap';
 
+import { faPlug, faBan } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import ConnectionStateMessage from './ConnectionStateMessage';
 import NbActionPending from './NbActionPending';
 
-import { faPlug, faBan } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import syncService from '../../services/SyncService';
 import actionManager from '../../services/ActionManager';
@@ -15,43 +15,47 @@ type ConnectionState = {
     isSynced: boolean
 };
 
-const ConnectionStateIcon = ({isOnline, isSynced}:ConnectionState) => {
-    if (isOnline){
-        return <FontAwesomeIcon icon={faPlug} />
-    }
-    else{
-        return <FontAwesomeIcon icon={faBan} />
-    }
-}
+const ConnectionStateIcon = ({ isOnline, isSynced }:ConnectionState) => {
+  if (isOnline) {
+    return <FontAwesomeIcon icon={faPlug} />;
+  }
+
+  return <FontAwesomeIcon icon={faBan} />;
+};
 
 const DropDownConnectionStateItem = () => {
-    const [isOnline, onIsOnlineChanged] = useState(false);
-    const [isSync, onIsSynced] = useState(false);
+  const [isOnline, onIsOnlineChanged] = useState(false);
+  const [isSync, onIsSynced] = useState(false);
 
-    const onActionManagerChanged = (nbAction: number): void => {
-        onIsSynced(nbAction === 0);
-    }
-    
-    useEffect(() => {
-        syncService.registerIsOnlineListener(onIsOnlineChanged);
-        onIsOnlineChanged(syncService.isOnline());
+  const onActionManagerChanged = (nbAction: number): void => {
+    onIsSynced(nbAction === 0);
+  };
 
-        actionManager.registerOnActionManagerChanged(onActionManagerChanged);
-        actionManager.countAction().then((nbAction: number) => {
-            onActionManagerChanged(nbAction);
-        });
+  useEffect(() => {
+    syncService.registerIsOnlineListener(onIsOnlineChanged);
+    onIsOnlineChanged(syncService.isOnline());
 
-        return () => {
-            syncService.unregisterIsOnlineListener(onIsOnlineChanged);
-            actionManager.unregisterOnActionManagerChanged(onActionManagerChanged);
-        }
-    }, []);
+    actionManager.registerOnActionManagerChanged(onActionManagerChanged);
+    actionManager.countAction().then((nbAction: number) => {
+      onActionManagerChanged(nbAction);
+    });
 
-	return (
-        <DropdownItem disabled={isOnline === false || isSync === true} onClick={syncService.synchronize}>
-            <ConnectionStateIcon isOnline={isOnline} isSynced={isSync}/>{' '}<ConnectionStateMessage isOnline={isOnline} isSynced={isSync}/>{' ('}<NbActionPending/>{')'}
-        </DropdownItem>
-	);
-}
+    return () => {
+      syncService.unregisterIsOnlineListener(onIsOnlineChanged);
+      actionManager.unregisterOnActionManagerChanged(onActionManagerChanged);
+    };
+  }, []);
+
+  return (
+    <DropdownItem disabled={isOnline === false || isSync === true} onClick={syncService.synchronize}>
+      <ConnectionStateIcon isOnline={isOnline} isSynced={isSync} />
+      {' '}
+      <ConnectionStateMessage isOnline={isOnline} isSynced={isSync} />
+     (
+      <NbActionPending />
+    )
+    </DropdownItem>
+  );
+};
 
 export default DropDownConnectionStateItem;
