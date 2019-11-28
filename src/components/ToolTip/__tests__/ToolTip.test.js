c14bdd6d2032bb4bd4c8c08a78dd9a2b7fa33cb7
Torruella, Paul <paul.torruella@suez.com>
2019-11-28 09:58:59 +0800

    eslint

--- src/components/ToolTip/__tests__/ToolTip.test.js
+++ src/components/ToolTip/__tests__/ToolTip.test.js
@@ -1,52 +1,49 @@
 import React from 'react';
 import { UncontrolledTooltip } from 'reactstrap';
-import { FormattedMessage } from 'react-intl';
 
-import {  mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
-import ToolTip from '../ToolTip'
+import ToolTip from '../ToolTip';
 
-describe('Component ToolTip', () =>{
-    let element;
-    let container;
+describe('Component ToolTip', () => {
+  let element;
+  let container;
 
-    beforeEach(() => {
-        element = document.createElement('div');
-        container = document.createElement('div');
-        element.innerHTML = '<p id="target">This is the tooltip <span id="innerTarget">target</span>.</p>';
-        element.setAttribute('id', 'testContainer');
-        container.setAttribute('id', 'container');
-        element.appendChild(container);
-        document.body.appendChild(element);
-    
-        jest.useFakeTimers();
-      });
+  beforeEach(() => {
+    element = document.createElement('div');
+    container = document.createElement('div');
+    element.innerHTML = '<p id="target">This is the tooltip <span id="innerTarget">target</span>.</p>';
+    element.setAttribute('id', 'testContainer');
+    container.setAttribute('id', 'container');
+    element.appendChild(container);
+    document.body.appendChild(element);
 
-    it('should render as a simple tooltip', () => {
-        // Arrange
-        const message = { id:"tooltip", defaultMessage: "hello world", description: "a tooltip"};
+    jest.useFakeTimers();
+  });
 
-        // Act
-        const wrapper = mount(<ToolTip tooltip={message}/>, { attachTo: container });
+  it('should render as a simple tooltip', () => {
+    // Arrange
+    const message = { id: 'tooltip', defaultMessage: 'hello world', description: 'a tooltip' };
 
-        // Assert
-        const innerToolTip = wrapper.find(UncontrolledTooltip);
-        const formatedMessage = innerToolTip.props().children;
-    
-        expect(formatedMessage.props.defaultMessage).toEqual(message.defaultMessage);
-        expect(formatedMessage.props.id).toEqual(message.id);
-    });
+    // Act
+    const wrapper = mount(<ToolTip tooltip={message} />, { attachTo: container });
 
-    it('should be the same as the previous snapshot', () => {
-        // Arrange
-        const message = { id:"tooltip", defaultMessage: "hello world", description: "a tooltip"};
+    // Assert
+    const innerToolTip = wrapper.find(UncontrolledTooltip);
+    const formatedMessage = innerToolTip.props().children;
 
-        // Act
-        const wrapper = mount(<ToolTip tooltip={message}/>, { attachTo: container });
+    expect(formatedMessage.props.defaultMessage).toEqual(message.defaultMessage);
+    expect(formatedMessage.props.id).toEqual(message.id);
+  });
 
-        // Assert
-        expect(wrapper).toMatchSnapshot();
-    });
+  it('should be the same as the previous snapshot', () => {
+    // Arrange
+    const message = { id: 'tooltip', defaultMessage: 'hello world', description: 'a tooltip' };
 
-    
+    // Act
+    const wrapper = mount(<ToolTip tooltip={message} />, { attachTo: container });
+
+    // Assert
+    expect(wrapper).toMatchSnapshot();
+  });
 });
