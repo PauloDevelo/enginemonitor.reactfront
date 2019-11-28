8a04e00d8a1bdd0072bc03ac623c4258908fbda8
Torruella, Paul <paul.torruella@suez.com>
2019-11-28 08:03:53 +0800

    eslint

--- src/services/__tests__/UserProxy.test.js
+++ src/services/__tests__/UserProxy.test.js
@@ -6,151 +6,142 @@ jest.mock('../HttpProxy');
 jest.mock('../StorageService');
 
 describe('Test UserProxy', () => {
-    beforeEach(() => {
-        storageService.setGlobalItem.mockReset();
-        storageService.removeGlobalItem.mockReset();
-        storageService.getGlobalItem.mockReset();
-        storageService.openUserStorage.mockReset();
-        storageService.closeUserStorage.mockReset();
+  beforeEach(() => {
+    storageService.setGlobalItem.mockReset();
+    storageService.removeGlobalItem.mockReset();
+    storageService.getGlobalItem.mockReset();
+    storageService.openUserStorage.mockReset();
+    storageService.closeUserStorage.mockReset();
 
-        httpProxy.setConfig.mockReset();
-        httpProxy.post.mockReset();
-    });
-    
-    describe('fetchCurrentUser', () => {
-        it("when the user is already authenticated in the storage service, it should set the config into the httpProxy for later request, open the user storage and return the user", async () => {
-            // arrange
-            const config = { headers: { Authorization: 'Token jwttoken' }};
+    httpProxy.setConfig.mockReset();
+    httpProxy.post.mockReset();
+  });
 
-            const user = {
-                email: 'test@axios',
-                firstname: 'jest',
-                name: 'react',
-                token: 'jwt',
-            };
+  describe('fetchCurrentUser', () => {
+    it('when the user is already authenticated in the storage service, it should set the config into the httpProxy for later request, open the user storage and return the user', async () => {
+      // arrange
+      const config = { headers: { Authorization: 'Token jwttoken' } };
 
-            storageService.getGlobalItem.mockImplementation((key) => {
-                if(key === 'EquipmentMonitorServiceProxy.config'){
-                    return Promise.resolve(config);
-                }
-                else if(key === 'currentUser'){
-                    return Promise.resolve(user);
-                }
-                else{
-                    return Promise.resolve(null);
-                }
-            });
+      const user = {
+        email: 'test@axios',
+        firstname: 'jest',
+        name: 'react',
+        token: 'jwt',
+      };
 
-            // act
-            const fetchedUser = await userProxy.fetchCurrentUser();
-            
-            // assert
-            expect(fetchedUser).toEqual(user);
-            expect(httpProxy.setConfig).toHaveBeenCalledTimes(1);
-            expect(storageService.openUserStorage).toHaveBeenCalledTimes(1);
-        });
+      storageService.getGlobalItem.mockImplementation((key) => {
+        if (key === 'EquipmentMonitorServiceProxy.config') {
+          return Promise.resolve(config);
+        }
+        if (key === 'currentUser') {
+          return Promise.resolve(user);
+        }
 
-        it("when there the user is not authenticated, it should return undefined and it should remove the config into the httpProxy", async () => {
-            // arrange
-            const config = undefined;
+        return Promise.resolve(null);
+      });
 
-            storageService.getGlobalItem.mockImplementation((key) => {
-                if(key === 'EquipmentMonitorServiceProxy.config'){
-                    return Promise.resolve(config);
-                }
-                else{
-                    return Promise.resolve(null);
-                }
-            });
+      // act
+      const fetchedUser = await userProxy.fetchCurrentUser();
 
-            // act
-            const fetchedUser = await userProxy.fetchCurrentUser();
-            
-            // assert
-            expect(fetchedUser).toEqual(undefined);
-            expect(httpProxy.setConfig).toHaveBeenCalledTimes(1);
-            expect(storageService.openUserStorage).toHaveBeenCalledTimes(0);
-        });
+      // assert
+      expect(fetchedUser).toEqual(user);
+      expect(httpProxy.setConfig).toHaveBeenCalledTimes(1);
+      expect(storageService.openUserStorage).toHaveBeenCalledTimes(1);
     });
 
-    describe('authenticate', () => {
-        it("should remove the config in httpProxy, delete global items into storage and close the user storage", async() => {
-            // arrange
+    it('when there the user is not authenticated, it should return undefined and it should remove the config into the httpProxy', async () => {
+      // arrange
+      const config = undefined;
 
-            // act
-            await userProxy.logout();
-            
-            // assert
-            expect(httpProxy.setConfig).toHaveBeenCalledTimes(1);
-            expect(storageService.closeUserStorage).toHaveBeenCalledTimes(1);
-            expect(storageService.removeGlobalItem).toHaveBeenCalledTimes(2);
-        });
+      storageService.getGlobalItem.mockImplementation((key) => {
+        if (key === 'EquipmentMonitorServiceProxy.config') {
+          return Promise.resolve(config);
+        }
 
-        it("should post the authentification, update the local storage and set the httpProxy config because of the remember flag it true", async() => {
-            // Arrange
-            const user = {
-                email: 'test@jest',
-                firstname: 'jest',
-                name: 'react',
-                token: 'jwt'
-            };
+        return Promise.resolve(null);
+      });
 
-            httpProxy.post.mockImplementationOnce(() =>
-                Promise.resolve(
-                    { user: user }
-                )
-            );
+      // act
+      const fetchedUser = await userProxy.fetchCurrentUser();
 
-            var currentConfig = undefined;
-            httpProxy.setConfig.mockImplementation((config) => {
-                currentConfig = config;
-            });
-
-            const authInfo = { email: "test@jest", password: "passwordTest", remember: true };
-
-            // Act
-            const authUser = await userProxy.authenticate(authInfo);
-
-            // Assert
-            expect(authUser).toEqual(user);
-            expect(currentConfig).toEqual({ headers: { Authorization: 'Token ' + user.token }});
-            expect(storageService.openUserStorage).toHaveBeenCalledTimes(1);
-            expect(storageService.setGlobalItem).toHaveBeenCalledTimes(2);
-        });
-
-        it("should not update the local storage because of the remember flag at false but should configure the http proxy and open the user storage.", async() => {
-            // Arrange
-            const user = {
-                email: 'test@jest',
-                firstname: 'jest',
-                name: 'react',
-                token: 'jwt'
-            };
-
-            httpProxy.post.mockImplementationOnce(() =>
-                Promise.resolve(
-                    { user: user }
-                )
-            );
-
-            var currentConfig = undefined;
-            httpProxy.setConfig.mockImplementation((config) => {
-                currentConfig = config;
-            });
-
-            const authInfo = { email: "test@jest", password: "passwordTest", remember: false };
-
-            // Act
-            const authUser = await userProxy.authenticate(authInfo);
-
-            // Assert
-            expect(authUser).toEqual(user);
-            expect(currentConfig).toEqual({ headers: { Authorization: 'Token ' + user.token }});
-            expect(storageService.openUserStorage).toHaveBeenCalledTimes(1);
-            expect(storageService.setGlobalItem).toHaveBeenCalledTimes(0);
-        });
+      // assert
+      expect(fetchedUser).toEqual(undefined);
+      expect(httpProxy.setConfig).toHaveBeenCalledTimes(1);
+      expect(storageService.openUserStorage).toHaveBeenCalledTimes(0);
     });
+  });
+
+  describe('authenticate', () => {
+    it('should remove the config in httpProxy, delete global items into storage and close the user storage', async () => {
+      // arrange
+
+      // act
+      await userProxy.logout();
+
+      // assert
+      expect(httpProxy.setConfig).toHaveBeenCalledTimes(1);
+      expect(storageService.closeUserStorage).toHaveBeenCalledTimes(1);
+      expect(storageService.removeGlobalItem).toHaveBeenCalledTimes(2);
+    });
+
+    it('should post the authentification, update the local storage and set the httpProxy config because of the remember flag it true', async () => {
+      // Arrange
+      const user = {
+        email: 'test@jest',
+        firstname: 'jest',
+        name: 'react',
+        token: 'jwt',
+      };
+
+      httpProxy.post.mockImplementationOnce(() => Promise.resolve(
+        { user },
+      ));
+
+      let currentConfig;
+      httpProxy.setConfig.mockImplementation((config) => {
+        currentConfig = config;
+      });
+
+      const authInfo = { email: 'test@jest', password: 'passwordTest', remember: true };
+
+      // Act
+      const authUser = await userProxy.authenticate(authInfo);
+
+      // Assert
+      expect(authUser).toEqual(user);
+      expect(currentConfig).toEqual({ headers: { Authorization: `Token ${user.token}` } });
+      expect(storageService.openUserStorage).toHaveBeenCalledTimes(1);
+      expect(storageService.setGlobalItem).toHaveBeenCalledTimes(2);
+    });
+
+    it('should not update the local storage because of the remember flag at false but should configure the http proxy and open the user storage.', async () => {
+      // Arrange
+      const user = {
+        email: 'test@jest',
+        firstname: 'jest',
+        name: 'react',
+        token: 'jwt',
+      };
+
+      httpProxy.post.mockImplementationOnce(() => Promise.resolve(
+        { user },
+      ));
+
+      let currentConfig;
+      httpProxy.setConfig.mockImplementation((config) => {
+        currentConfig = config;
+      });
+
+      const authInfo = { email: 'test@jest', password: 'passwordTest', remember: false };
+
+      // Act
+      const authUser = await userProxy.authenticate(authInfo);
+
+      // Assert
+      expect(authUser).toEqual(user);
+      expect(currentConfig).toEqual({ headers: { Authorization: `Token ${user.token}` } });
+      expect(storageService.openUserStorage).toHaveBeenCalledTimes(1);
+      expect(storageService.setGlobalItem).toHaveBeenCalledTimes(0);
+    });
+  });
 });
-
-
-
