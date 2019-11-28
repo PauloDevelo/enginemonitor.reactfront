92a6d6d0c772b3c38819be380618baca7d637e5d
Torruella, Paul <paul.torruella@suez.com>
2019-11-28 09:51:08 +0800

    eslint

--- src/components/TaskTabPanes/MyNavItem.tsx
+++ src/components/TaskTabPanes/MyNavItem.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import { NavItem, NavLink } from 'reactstrap';
+// eslint-disable-next-line no-unused-vars
 import { FormattedMessage, MessageDescriptor } from 'react-intl';
 
 type Props = {
     classNames: string,
-    activeFunc: () => void, 
+    activeFunc: () => void,
     label: MessageDescriptor
 };
 
-const MyNavItem = ({classNames, activeFunc, label}:Props) => {
-    return <NavItem>
-                <NavLink className={classNames} 
-                    onClick={activeFunc}>
-                    <FormattedMessage {...label}/>
-                </NavLink>
-            </NavItem>
-} 
+const MyNavItem = ({ classNames, activeFunc, label }:Props) => (
+  <NavItem>
+    <NavLink
+      className={classNames}
+      onClick={activeFunc}
+    >
+      <FormattedMessage {...label} />
+    </NavLink>
+  </NavItem>
+);
 
 export default React.memo(MyNavItem);
