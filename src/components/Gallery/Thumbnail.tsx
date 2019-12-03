6c9f34aa58753a75e1ad205cd80032b9e45ace3d
Torruella, Paul <paul.torruella@suez.com>
2019-12-03 08:54:01 +0800

    eslint

--- src/components/Gallery/Thumbnail.tsx
+++ src/components/Gallery/Thumbnail.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Media } from 'reactstrap';
 
+// eslint-disable-next-line no-unused-vars
 import { ImageModel } from '../../types/Types';
 import './Thumbnail.css';
 
@@ -8,16 +9,16 @@ type Props = {
     image: ImageModel;
     onClickImage: undefined | (() => void);
 }
-function Thumbnail({image, onClickImage}: Props){
-    const displayImage = () => {
-        if (onClickImage !== undefined){
-            onClickImage();
-        }
+function Thumbnail({ image, onClickImage }: Props) {
+  const displayImage = () => {
+    if (onClickImage !== undefined) {
+      onClickImage();
     }
+  };
 
-    const alt  = image.title + " - " + image.description;
+  const alt = `${image.title} - ${image.description}`;
 
-    return <Media object src={ image.thumbnailUrl } alt={alt} onClick={displayImage} className={"thumbnail grow"}/>
+  return <Media object src={image.thumbnailUrl} alt={alt} onClick={displayImage} className="thumbnail grow" />;
 }
 
 export default React.memo(Thumbnail);
