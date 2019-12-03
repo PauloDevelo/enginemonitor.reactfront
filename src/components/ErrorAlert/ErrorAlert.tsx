461fd3830c77ffa092c0a5248426c892077c148c
Torruella, Paul <paul.torruella@suez.com>
2019-12-03 22:02:34 +0800

    eslint

--- src/components/ErrorAlert/ErrorAlert.tsx
+++ src/components/ErrorAlert/ErrorAlert.tsx
@@ -1,11 +1,15 @@
-import React, { Fragment, useState, useEffect, ReactElement } from 'react';
-import { FormattedMessage } from 'react-intl';
+import React, {
+  // eslint-disable-next-line no-unused-vars
+  useState, useEffect, ReactElement,
+} from 'react';
+import { FormattedMessage, defineMessages } from 'react-intl';
 
-import {Alert} from 'reactstrap';
+import { Alert } from 'reactstrap';
 import HttpError from '../../http/HttpError';
 
-import {defineMessages} from "react-intl";
-import jsonMessages from "./ErrorAlert.messages.json";
+
+import jsonMessages from './ErrorAlert.messages.json';
+
 const alertMsg = defineMessages(jsonMessages);
 
 type Props = {
@@ -14,42 +18,39 @@ type Props = {
     onDismiss: () => void;
 }
 
-const ErrorAlert = ({error, onDismiss, className}:Props) => {
-    const [isVisible, setVisible] = useState(error !== undefined);
+const ErrorAlert = ({ error, onDismiss, className }:Props) => {
+  const [isVisible, setVisible] = useState(error !== undefined);
 
-    useEffect(() => {
-        setVisible(error !== undefined)
-    }, [error]);
+  useEffect(() => {
+    setVisible(error !== undefined);
+  }, [error]);
 
-    let message:string = "";
-    if(error !== undefined){
-        if(error instanceof HttpError)
-        {
-            message = error.data.message;
-        }
-        else{
-            message = error.message;
-        }
+  let message:string = '';
+  if (error !== undefined) {
+    if (error instanceof HttpError) {
+      message = error.data.message;
+    } else {
+      message = error.message;
     }
+  }
 
-    let msgElement: string | ReactElement;
+  let msgElement: string | ReactElement;
 
-    const messageDescIndex = Object.keys(alertMsg).indexOf(message);
-    if(messageDescIndex !== -1){
-        const messageDesc = Object.values(alertMsg)[messageDescIndex];
-        msgElement = <FormattedMessage {...messageDesc}/>;
-    }
-    else{
-        msgElement = message;
-    }
+  const messageDescIndex = Object.keys(alertMsg).indexOf(message);
+  if (messageDescIndex !== -1) {
+    const messageDesc = Object.values(alertMsg)[messageDescIndex];
+    msgElement = <FormattedMessage {...messageDesc} />;
+  } else {
+    msgElement = message;
+  }
 
-    return (
-        <Fragment>
-            <Alert color="danger" className={className} isOpen={isVisible} toggle={onDismiss}>
-                <div className="text-center">{msgElement}</div>
-            </Alert>
-        </Fragment>
-    )
-}
+  return (
+    <>
+      <Alert color="danger" className={className} isOpen={isVisible} toggle={onDismiss}>
+        <div className="text-center">{msgElement}</div>
+      </Alert>
+    </>
+  );
+};
 
 export default React.memo(ErrorAlert);
