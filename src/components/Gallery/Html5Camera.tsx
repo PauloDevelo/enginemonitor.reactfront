76db8c8ae25428b2570cc6e593892d59e93c947a
Torruella, Paul <paul.torruella@suez.com>
2019-12-17 08:53:15 +0800

    eslint

--- src/components/Gallery/Html5Camera.tsx
+++ src/components/Gallery/Html5Camera.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 import Camera, { FACING_MODES, IMAGE_TYPES } from 'react-html5-camera-photo';
 import 'react-html5-camera-photo/build/css/index.css';
-import "./Html5Camera.css"
+import './Html5Camera.css';
 
-import {resizeAndSaveBase64Image} from '../../helpers/ImageHelper';
+import { resizeAndSaveBase64Image } from '../../helpers/ImageHelper';
 
+// eslint-disable-next-line no-unused-vars
 import { ImageModel } from '../../types/Types';
 
 import errorService from '../../services/ErrorService';
@@ -16,54 +17,52 @@ type Props = {
     close: ()=> void;
 }
 
-const Html5Camera = ({imageParentUiId, addImage, close}: Props) => {
+const Html5Camera = ({ imageParentUiId, addImage, close }: Props) => {
+  const onCameraError = () => {
+    close();
+  };
 
-    const onCameraError = (error: any) => {
-        close();
-    }
- 
-    const onCameraStart = (stream: MediaStream) => {
-    }
- 
-    const onCameraStop = () => {
-        close();
-    }
+  const onCameraStart = () => {
+  };
 
-    const onTakePhoto = async (dataUri: string, method: string) => {
-        try{
-            if(method === "multer"){
-                const newImage = await resizeAndSaveBase64Image(dataUri, imageParentUiId);
-                addImage(newImage);
-            }
-        }
-        catch(error){
-            errorService.addError(error);
-        }
+  const onCameraStop = () => {
+    close();
+  };
+
+  const onTakePhoto = async (dataUri: string, method: string) => {
+    try {
+      if (method === 'multer') {
+        const newImage = await resizeAndSaveBase64Image(dataUri, imageParentUiId);
+        addImage(newImage);
+      }
+    } catch (error) {
+      errorService.addError(error);
     }
- 
-    return (
-        <div id={"cameraContainer"} onClick={close}>
-            <Camera
-                onTakePhoto = { (dataUri: string) => {
-                    onTakePhoto(dataUri, "multer");
-                    close();
-                }}
-                onCameraError = { (error: any) => onCameraError(error) }
-                idealFacingMode = {FACING_MODES.ENVIRONMENT}
-                idealResolution = {{width: 1024, height: 1024}}
-                imageType = {IMAGE_TYPES.JPG}
-                imageCompression = {0.92}
-                isMaxResolution = {false}
-                isImageMirror = {false}
-                isSilentMode = {false}
-                isDisplayStartCameraError = {true}
-                isFullscreen = {false}
-                sizeFactor = {1}
-                onCameraStart = { (stream: MediaStream) => onCameraStart(stream) }
-                onCameraStop = { () => onCameraStop() }
-            />
-        </div>
-    );
-}
+  };
+
+  return (
+    <div id="cameraContainer" onClick={close}>
+      <Camera
+        onTakePhoto={(dataUri: string) => {
+          onTakePhoto(dataUri, 'multer');
+          close();
+        }}
+        onCameraError={() => onCameraError()}
+        idealFacingMode={FACING_MODES.ENVIRONMENT}
+        idealResolution={{ width: 1024, height: 1024 }}
+        imageType={IMAGE_TYPES.JPG}
+        imageCompression={0.92}
+        isMaxResolution={false}
+        isImageMirror={false}
+        isSilentMode={false}
+        isDisplayStartCameraError
+        isFullscreen={false}
+        sizeFactor={1}
+        onCameraStart={() => onCameraStart()}
+        onCameraStop={() => onCameraStop()}
+      />
+    </div>
+  );
+};
 
 export default React.memo(Html5Camera);
