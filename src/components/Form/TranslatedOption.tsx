d7384129159d0d07d44b756ca28451eeb3550af4
Torruella, Paul <paul.torruella@suez.com>
2019-12-17 08:49:44 +0800

    eslint

--- src/components/Form/TranslatedOption.tsx
+++ src/components/Form/TranslatedOption.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+
+// eslint-disable-next-line no-unused-vars
 import { MessageDescriptor, injectIntl, IntlShape } from 'react-intl';
 
 type Props = {
@@ -7,12 +9,15 @@ type Props = {
     intl: IntlShape
 }
 
-const TranslatedOption = ({value, message, intl}:Props) => {
-    const {formatMessage} = intl;
+const TranslatedOption = ({ value, message, intl }:Props) => {
+  const { formatMessage } = intl;
 
-    return (
-        <option value={value}> {formatMessage(message)}</option>
-    );
+  return (
+    <option value={value}>
+      {' '}
+      {formatMessage(message)}
+    </option>
+  );
 };
 
 export default injectIntl(TranslatedOption);
--- src/components/Form/__tests__/MyInput.test.js
+++ src/components/Form/__tests__/MyInput.test.js
@@ -1,44 +1,44 @@
-import ignoredMessages from '../../../testHelpers/MockConsole';
 import React from 'react';
 import { mount } from 'enzyme';
+import ignoredMessages from '../../../testHelpers/MockConsole';
 
-import MyInput from '../MyInput'
+import MyInput from '../MyInput';
 
 const defaultMessage = {
-    id: "defaultId",
-    defaultMessage: "Default message",
-    description: "Label"
+  id: 'defaultId',
+  defaultMessage: 'Default message',
+  description: 'Label',
 };
 
-describe("MyInput", () => {
-    beforeAll(() => {
-        ignoredMessages.length = 0;
-        ignoredMessages.push("[React Intl] Could not find required `intl` object. <IntlProvider> needs to exist in the component ancestry. Using default message as fallback.");
-    });
+describe('MyInput', () => {
+  beforeAll(() => {
+    ignoredMessages.length = 0;
+    ignoredMessages.push('[React Intl] Could not find required `intl` object. <IntlProvider> needs to exist in the component ancestry. Using default message as fallback.');
+  });
 
-    it("of type text should render as expected", () => {
-        // Arrange
+  it('of type text should render as expected', () => {
+    // Arrange
 
-        const wrapper = mount(<MyInput name="name" 	label={defaultMessage} 	    type="text" 	required/>);
+    const wrapper = mount(<MyInput name="name" label={defaultMessage} type="text" required />);
 
-        // Assert
-        expect(wrapper).toMatchSnapshot();
-        expect(wrapper.find('label').text()).toEqual(defaultMessage.defaultMessage);
-        expect(wrapper.find('input').prop('required')).toEqual(true);
-        expect(wrapper.find('input').prop('type')).toEqual('text');
-        expect(wrapper.find('input').prop('name')).toEqual('name');
-    });
+    // Assert
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('label').text()).toEqual(defaultMessage.defaultMessage);
+    expect(wrapper.find('input').prop('required')).toEqual(true);
+    expect(wrapper.find('input').prop('type')).toEqual('text');
+    expect(wrapper.find('input').prop('name')).toEqual('name');
+  });
 
-    it("of type checkox should render as expected", () => {
-        // Arrange
+  it('of type checkox should render as expected', () => {
+    // Arrange
 
-        const wrapper = mount(<MyInput name="name" 	label={defaultMessage} 	    type="checkbox" 	required/>);
+    const wrapper = mount(<MyInput name="name" label={defaultMessage} type="checkbox" required />);
 
-        // Assert
-        expect(wrapper).toMatchSnapshot();
-        expect(wrapper.find('label').text()).toEqual(defaultMessage.defaultMessage + ' ');
-        expect(wrapper.find('input').prop('required')).toEqual(true);
-        expect(wrapper.find('input').prop('type')).toEqual('checkbox');
-        expect(wrapper.find('input').prop('name')).toEqual('name');
-    }); 
+    // Assert
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('label').text()).toEqual(`${defaultMessage.defaultMessage} `);
+    expect(wrapper.find('input').prop('required')).toEqual(true);
+    expect(wrapper.find('input').prop('type')).toEqual('checkbox');
+    expect(wrapper.find('input').prop('name')).toEqual('name');
+  });
 });
