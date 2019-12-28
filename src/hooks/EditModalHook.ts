7a15616156870f87b942775de5c012fdf28eb835
Torruella, Paul <paul.torruella@suez.com>
2019-12-28 14:12:27 +0800

    eslint

--- src/hooks/EditModalHook.ts
+++ src/hooks/EditModalHook.ts
@@ -1,17 +1,19 @@
 import { useState } from 'react';
 
-export function useEditModal<T>(initialData: T){
-    const [data, setData] = useState(initialData);
-    const [editModalVisibility, setEditModalVisibility] = useState(false);
-    
-    const toggleModal = () => {
-        setEditModalVisibility(!editModalVisibility);
-    }
+export default function useEditModal<T>(initialData: T) {
+  const [data, setData] = useState(initialData);
+  const [editModalVisibility, setEditModalVisibility] = useState(false);
 
-    const displayData = (data: T)=>{
-        setData(data);
-        setEditModalVisibility(true);
-    }
-  
-    return { data, editModalVisibility, toggleModal, displayData, setData };
+  const toggleModal = () => {
+    setEditModalVisibility(!editModalVisibility);
   };
+
+  const displayData = (dataToDisplay: T) => {
+    setData(dataToDisplay);
+    setEditModalVisibility(true);
+  };
+
+  return {
+    data, editModalVisibility, toggleModal, displayData, setData,
+  };
+}
