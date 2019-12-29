285a52060c0a1810421769113c79e95076801386
Torruella, Paul <paul.torruella@suez.com>
2019-12-29 15:51:38 +0800

    Add the progressive function postNewImage into ProgressiveHttpProxy. Add the management of the aborted connection exception in the ProgressiveHttpProxy and use the storage as a fallback.

--- src/services/ProgressiveHttpProxy.ts
+++ src/services/ProgressiveHttpProxy.ts
@@ -2,9 +2,12 @@
 
 // eslint-disable-next-line no-unused-vars
 import { CancelToken } from 'axios';
+import HttpError from '../http/HttpError';
+// eslint-disable-next-line no-unused-vars
+import { ImageModel } from '../types/Types';
+
 import syncService from './SyncService';
 import httpProxy from './HttpProxy';
-
 // eslint-disable-next-line no-unused-vars
 import actionManager, { Action, ActionType } from './ActionManager';
 import storageService from './StorageService';
@@ -13,6 +16,15 @@ import storageService from './StorageService';
  * This interface is an enhanced http proxy that manages offline mode.
  */
 export interface ISyncHttpProxy{
+    /**
+     * This function will try to post a new image into a back end. If it cannot, the image creation will be added into the action manager.
+     * @param createImageUrl Url to post the new image
+     * @param imgToSave image data model to save
+     * @param blobImage Blob containing the image
+     * @param thumbnail Blob containing the thumbnail
+     */
+    postNewImage(createImageUrl: string, imgToSave: ImageModel, blobImage: Blob, thumbnail: Blob): Promise<ImageModel>;
+
     /**
      * This function execute an http post query and call the function update in the element's "keyname" field returned by the query.
      * In offline mode, this function will add an action in the history.
@@ -52,39 +64,94 @@ export interface ISyncHttpProxy{
 }
 
 class ProgressiveHttpProxy implements ISyncHttpProxy {
+  async postNewImage(createImageUrl: string, imgToSave: ImageModel, blobImage: Blob, thumbnail: Blob): Promise<ImageModel> {
+    const addCreateImageAction = async () => {
+      const action: Action = { key: createImageUrl, type: ActionType.CreateImage, data: imgToSave };
+      await actionManager.addAction(action);
+    };
+
+    if (await syncService.isOnlineAndSynced()) {
+      try {
+        const { image } = await httpProxy.postImage(createImageUrl, imgToSave, blobImage, thumbnail);
+        return image;
+      } catch (reason) {
+        if (reason instanceof HttpError && reason.didConnectionAbort()) {
+          await addCreateImageAction();
+        } else {
+          throw reason;
+        }
+      }
+    } else {
+      await addCreateImageAction();
+    }
+
+    return { ...imgToSave, sizeInByte: 450 * 1024 };
+  }
+
   async postAndUpdate<T>(url: string, keyName:string, dataToPost:T, update:(data:T)=>T):Promise<T> {
     const data:any = { [keyName]: dataToPost };
 
+    const addPostAction = async () => {
+      const action: Action = { key: url, type: ActionType.Post, data };
+      await actionManager.addAction(action);
+    };
+
     if (await syncService.isOnlineAndSynced()) {
-      const savedData = (await httpProxy.post(url, data))[keyName];
+      try {
+        const savedData = (await httpProxy.post(url, data))[keyName];
 
-      return update(savedData);
+        return update(savedData);
+      } catch (reason) {
+        if (reason instanceof HttpError && reason.didConnectionAbort()) {
+          await addPostAction();
+        } else {
+          throw reason;
+        }
+      }
+    } else {
+      await addPostAction();
     }
-
-    const action: Action = { key: url, type: ActionType.Post, data };
-    await actionManager.addAction(action);
 
     return dataToPost;
   }
 
   async deleteAndUpdate<T>(url: string, keyName: string, update:(data:T)=>T):Promise<void> {
-    if (await syncService.isOnlineAndSynced()) {
-      const deletedEntity = (await httpProxy.deleteReq(url))[keyName];
-      update(deletedEntity);
-    } else {
+    const addDeleteAction = () => {
       const action: Action = { key: url, type: ActionType.Delete };
       actionManager.addAction(action);
+    };
+
+    if (await syncService.isOnlineAndSynced()) {
+      try {
+        const deletedEntity = (await httpProxy.deleteReq(url))[keyName];
+        update(deletedEntity);
+      } catch (reason) {
+        if (reason instanceof HttpError && reason.didConnectionAbort()) {
+          addDeleteAction();
+        } else {
+          throw reason;
+        }
+      }
+    } else {
+      addDeleteAction();
     }
   }
 
   async getArrayOnlineFirst<T>(url: string, keyName:string, init:(model:T) => T, cancelToken: CancelToken | undefined = undefined): Promise<T[]> {
     if (await syncService.isOnlineAndSynced()) {
-      const array = (await httpProxy.get(url, cancelToken))[keyName] as T[];
-      const initArray = array.map(init);
+      try {
+        const array = (await httpProxy.get(url, cancelToken))[keyName] as T[];
+        const initArray = array.map(init);
 
-      storageService.setItem<T[]>(url, initArray);
+        storageService.setItem<T[]>(url, initArray);
 
-      return initArray;
+        return initArray;
+      } catch (reason) {
+        if (reason instanceof HttpError && reason.didConnectionAbort()) {
+          return storageService.getArray<T>(url);
+        }
+        throw reason;
+      }
     }
 
     return storageService.getArray<T>(url);
@@ -92,12 +159,19 @@ class ProgressiveHttpProxy implements ISyncHttpProxy {
 
   async getOnlineFirst<T>(url: string, keyName:string, init:(model:T) => T, cancelToken: CancelToken | undefined = undefined): Promise<T> {
     if (await syncService.isOnlineAndSynced()) {
-      const item = (await httpProxy.get(url, cancelToken))[keyName] as T;
-      const updatedItem = init(item);
+      try {
+        const item = (await httpProxy.get(url, cancelToken))[keyName] as T;
+        const updatedItem = init(item);
 
-      storageService.setItem<T>(url, updatedItem);
+        storageService.setItem<T>(url, updatedItem);
 
-      return updatedItem;
+        return updatedItem;
+      } catch (reason) {
+        if (reason instanceof HttpError && reason.didConnectionAbort()) {
+          return storageService.getItem<T>(url);
+        }
+        throw reason;
+      }
     }
 
     return storageService.getItem<T>(url);
---
+++ src/services/__tests__/ProgressiveHttpProxy.test.js
@@ -0,0 +1,283 @@
+import HttpError from '../../http/HttpError';
+import httpProxy from '../HttpProxy';
+import storageService from '../StorageService';
+import syncService from '../SyncService';
+import actionManager, { ActionType } from '../ActionManager';
+
+import progressiveHttpProxy from '../ProgressiveHttpProxy';
+
+jest.mock('../HttpProxy');
+jest.mock('../StorageService');
+jest.mock('../SyncService');
+jest.mock('../ActionManager');
+
+describe('Test ProgressiveHttpProxy', () => {
+  function resetMockHttpProxy() {
+    httpProxy.setConfig.mockReset();
+    httpProxy.post.mockReset();
+    httpProxy.deleteReq.mockReset();
+    httpProxy.get.mockReset();
+    httpProxy.postImage.mockReset();
+  }
+
+  function resetMockStorageService() {
+    storageService.setItem.mockReset();
+    storageService.removeItem.mockReset();
+    storageService.updateArray.mockReset();
+    storageService.removeItemInArray.mockReset();
+    storageService.getArray.mockReset();
+    storageService.getItem.mockReset();
+  }
+
+  function resetMockSyncService() {
+    syncService.isOnlineAndSynced.mockReset();
+  }
+
+  function resetMockActionManager() {
+    actionManager.addAction.mockReset();
+  }
+
+  beforeEach(() => {
+  });
+
+  afterEach(async () => {
+    resetMockHttpProxy();
+    resetMockStorageService();
+    resetMockSyncService();
+    resetMockActionManager();
+  });
+
+  const postAndUpdateItems = [
+    {
+      throwAnHttpConnAborted: false, isOnlineAndSync: true, keyname: 'keyname', addActionNbCall: 0, expectedHttpPostCall: 1,
+    },
+    {
+      throwAnHttpConnAborted: false, isOnlineAndSync: false, keyname: 'keyname', addActionNbCall: 1, expectedHttpPostCall: 0,
+    },
+    {
+      throwAnHttpConnAborted: true, isOnlineAndSync: true, keyname: 'keyname', addActionNbCall: 1, expectedHttpPostCall: 1,
+    },
+  ];
+  describe.each(postAndUpdateItems)('postAndUpdate', ({
+    throwAnHttpConnAborted, isOnlineAndSync, keyname, addActionNbCall, expectedHttpPostCall,
+  }) => {
+    it('shoud call the http proxy or add the expected action in action manager', async () => {
+      // Arrange
+      const dataToPost = { data: 'some data' };
+      const urlToPost = 'an_url';
+      const updateFn = jest.fn((data) => data);
+
+      jest.spyOn(httpProxy, 'post').mockImplementation(async (url, data) => {
+        if (throwAnHttpConnAborted) {
+          const axiosError = new Error('timeout');
+          axiosError.code = 'ECONNABORTED';
+
+          throw new HttpError(axiosError.message, axiosError);
+        }
+
+        return Promise.resolve(data);
+      });
+
+      syncService.isOnlineAndSynced.mockImplementation(async () => Promise.resolve(isOnlineAndSync));
+
+      jest.spyOn(actionManager, 'addAction');
+
+      // Act
+      const dataPosted = await progressiveHttpProxy.postAndUpdate(urlToPost, keyname, dataToPost, updateFn);
+
+      // Assert
+      expect(dataPosted).toStrictEqual(dataToPost);
+
+      expect(httpProxy.post).toBeCalledTimes(expectedHttpPostCall);
+      if (expectedHttpPostCall > 0) {
+        expect(httpProxy.post.mock.calls[0][0]).toStrictEqual(urlToPost);
+        expect(httpProxy.post.mock.calls[0][1][keyname]).toStrictEqual(dataToPost);
+      }
+
+      expect(actionManager.addAction).toBeCalledTimes(addActionNbCall);
+      if (addActionNbCall > 0) {
+        expect(actionManager.addAction.mock.calls[0][0].key).toStrictEqual(urlToPost);
+        expect(actionManager.addAction.mock.calls[0][0].type).toStrictEqual(ActionType.Post);
+        expect(actionManager.addAction.mock.calls[0][0].data[keyname]).toStrictEqual(dataToPost);
+      }
+    });
+  });
+
+  const deleteAndUpdateItems = [
+    {
+      throwAnHttpConnAborted: false, isOnlineAndSync: true, keyname: 'keyname', addActionNbCall: 0, expectedHttpDeleteCall: 1,
+    },
+    {
+      throwAnHttpConnAborted: false, isOnlineAndSync: false, keyname: 'keyname', addActionNbCall: 1, expectedHttpDeleteCall: 0,
+    },
+    {
+      throwAnHttpConnAborted: true, isOnlineAndSync: true, keyname: 'keyname', addActionNbCall: 1, expectedHttpDeleteCall: 1,
+    },
+  ];
+  describe.each(deleteAndUpdateItems)('deleteAndUpdate', ({
+    throwAnHttpConnAborted, isOnlineAndSync, keyname, addActionNbCall, expectedHttpDeleteCall,
+  }) => {
+    it('shoud call the http proxy or add the expected action in action manager', async () => {
+      // Arrange
+      const dataToDelete = { data: 'some data' };
+      const urlToDelete = 'an_url';
+      const updateFn = jest.fn((data) => data);
+
+      jest.spyOn(httpProxy, 'deleteReq').mockImplementation(async () => {
+        if (throwAnHttpConnAborted) {
+          const axiosError = new Error('timeout');
+          axiosError.code = 'ECONNABORTED';
+
+          throw new HttpError(axiosError.message, axiosError);
+        }
+
+        const dataToReturn = {};
+        dataToReturn[keyname] = dataToDelete;
+        return Promise.resolve(dataToReturn);
+      });
+
+      syncService.isOnlineAndSynced.mockImplementation(async () => Promise.resolve(isOnlineAndSync));
+
+      jest.spyOn(actionManager, 'addAction');
+
+      // Act
+      await progressiveHttpProxy.deleteAndUpdate(urlToDelete, keyname, updateFn);
+
+      // Assert
+      expect(httpProxy.deleteReq).toBeCalledTimes(expectedHttpDeleteCall);
+      if (expectedHttpDeleteCall > 0) {
+        expect(httpProxy.deleteReq.mock.calls[0][0]).toStrictEqual(urlToDelete);
+      }
+
+      expect(actionManager.addAction).toBeCalledTimes(addActionNbCall);
+      if (addActionNbCall > 0) {
+        expect(actionManager.addAction.mock.calls[0][0].key).toStrictEqual(urlToDelete);
+        expect(actionManager.addAction.mock.calls[0][0].type).toStrictEqual(ActionType.Delete);
+      }
+    });
+  });
+
+  const getItemOnlineFirstItems = [
+    {
+      throwAnHttpConnAborted: false, isOnlineAndSync: true, keyname: 'keyname', expectedGetItemCall: 0, expectedHttpGetCall: 1, expectedSetItemCall: 1,
+    },
+    {
+      throwAnHttpConnAborted: false, isOnlineAndSync: false, keyname: 'keyname', expectedGetItemCall: 1, expectedHttpGetCall: 0, expectedSetItemCall: 0,
+    },
+    {
+      throwAnHttpConnAborted: true, isOnlineAndSync: true, keyname: 'keyname', expectedGetItemCall: 1, expectedHttpGetCall: 1, expectedSetItemCall: 0,
+    },
+  ];
+  describe.each(getItemOnlineFirstItems)('getOnlineFirst', ({
+    throwAnHttpConnAborted, isOnlineAndSync, keyname, expectedGetItemCall, expectedHttpGetCall, expectedSetItemCall,
+  }) => {
+    it('shoud call the http proxy or call the storageService', async () => {
+      // Arrange
+      const dataToGet = { data: 'some data' };
+      const urlToGet = 'an_url';
+      const updateFn = jest.fn((data) => data);
+
+      jest.spyOn(httpProxy, 'get').mockImplementation(async () => {
+        if (throwAnHttpConnAborted) {
+          const axiosError = new Error('timeout');
+          axiosError.code = 'ECONNABORTED';
+
+          throw new HttpError(axiosError.message, axiosError);
+        }
+
+        const dataToReturn = {};
+        dataToReturn[keyname] = dataToGet;
+        return Promise.resolve(dataToReturn);
+      });
+
+      syncService.isOnlineAndSynced.mockImplementation(async () => Promise.resolve(isOnlineAndSync));
+
+      jest.spyOn(storageService, 'getItem').mockImplementation(async () => Promise.resolve(dataToGet));
+      jest.spyOn(storageService, 'setItem');
+
+      // Act
+      const data = await progressiveHttpProxy.getOnlineFirst(urlToGet, keyname, updateFn);
+
+      // Assert
+      expect(data).toStrictEqual(dataToGet);
+
+      expect(httpProxy.get).toBeCalledTimes(expectedHttpGetCall);
+      if (expectedHttpGetCall > 0) {
+        expect(httpProxy.get.mock.calls[0][0]).toStrictEqual(urlToGet);
+      }
+
+      expect(storageService.setItem).toBeCalledTimes(expectedSetItemCall);
+      if (expectedSetItemCall > 0) {
+        expect(storageService.setItem.mock.calls[0][0]).toStrictEqual(urlToGet);
+        expect(storageService.setItem.mock.calls[0][1]).toStrictEqual(dataToGet);
+      }
+
+      expect(storageService.getItem).toBeCalledTimes(expectedGetItemCall);
+      if (expectedGetItemCall > 0) {
+        expect(storageService.getItem.mock.calls[0][0]).toStrictEqual(urlToGet);
+      }
+    });
+  });
+
+  const getArrayOnlineFirstItems = [
+    {
+      throwAnHttpConnAborted: false, isOnlineAndSync: true, keyname: 'keyname', expectedGetArrayCall: 0, expectedHttpGetCall: 1, expectedSetItemCall: 1,
+    },
+    {
+      throwAnHttpConnAborted: false, isOnlineAndSync: false, keyname: 'keyname', expectedGetArrayCall: 1, expectedHttpGetCall: 0, expectedSetItemCall: 0,
+    },
+    {
+      throwAnHttpConnAborted: true, isOnlineAndSync: true, keyname: 'keyname', expectedGetArrayCall: 1, expectedHttpGetCall: 1, expectedSetItemCall: 0,
+    },
+  ];
+  describe.each(getArrayOnlineFirstItems)('getArrayOnlineFirst', ({
+    throwAnHttpConnAborted, isOnlineAndSync, keyname, expectedGetArrayCall, expectedHttpGetCall, expectedSetItemCall,
+  }) => {
+    it('shoud call the http proxy or call the storageService', async () => {
+      // Arrange
+      const dataToGet = [{ data: 'some data' }];
+      const urlToGet = 'an_url';
+      const updateFn = jest.fn((data) => data);
+
+      jest.spyOn(httpProxy, 'get').mockImplementation(async () => {
+        if (throwAnHttpConnAborted) {
+          const axiosError = new Error('timeout');
+          axiosError.code = 'ECONNABORTED';
+
+          throw new HttpError(axiosError.message, axiosError);
+        }
+
+        const dataToReturn = {};
+        dataToReturn[keyname] = dataToGet;
+        return Promise.resolve(dataToReturn);
+      });
+
+      syncService.isOnlineAndSynced.mockImplementation(async () => Promise.resolve(isOnlineAndSync));
+
+      jest.spyOn(storageService, 'getArray').mockImplementation(async () => Promise.resolve(dataToGet));
+      jest.spyOn(storageService, 'setItem');
+
+      // Act
+      const data = await progressiveHttpProxy.getArrayOnlineFirst(urlToGet, keyname, updateFn);
+
+      // Assert
+      expect(data).toStrictEqual(dataToGet);
+
+      expect(httpProxy.get).toBeCalledTimes(expectedHttpGetCall);
+      if (expectedHttpGetCall > 0) {
+        expect(httpProxy.get.mock.calls[0][0]).toStrictEqual(urlToGet);
+      }
+
+      expect(storageService.setItem).toBeCalledTimes(expectedSetItemCall);
+      if (expectedSetItemCall > 0) {
+        expect(storageService.setItem.mock.calls[0][0]).toStrictEqual(urlToGet);
+        expect(storageService.setItem.mock.calls[0][1]).toStrictEqual(dataToGet);
+      }
+
+      expect(storageService.getArray).toBeCalledTimes(expectedGetArrayCall);
+      if (expectedGetArrayCall > 0) {
+        expect(storageService.getArray.mock.calls[0][0]).toStrictEqual(urlToGet);
+      }
+    });
+  });
+});
