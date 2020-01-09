3633c147c33c0635d1a50d744fb450af1bb0e260
Torruella, Paul <paul.torruella@suez.com>
2020-01-09 08:30:33 +0530

    eslint

--- src/helpers/EntryHelper.ts
+++ src/helpers/EntryHelper.ts
@@ -1,25 +1,30 @@
-import {EquipmentModel, TaskModel, EntryModel, AgeAcquisitionType} from '../types/Types'
-import uuidv1 from 'uuid/v1';
+import uuidv1 from 'uuid/v1.js';
 
-export function createDefaultEntry(equipment:EquipmentModel, task: TaskModel | undefined): EntryModel{
-	const uuid = uuidv1();
-	let defaultAge = -1;
-	if(equipment.ageAcquisitionType !== AgeAcquisitionType.time){
-		defaultAge = equipment.age;
-	}
+import {
+  // eslint-disable-next-line no-unused-vars
+  EquipmentModel, TaskModel, EntryModel, AgeAcquisitionType,
+} from '../types/Types';
 
-	return {
-		_uiId: uuid,
-		name: task?task.name:"",
-		date: new Date(),
-		age: defaultAge,
-		remarks: '',
-		taskUiId: task?task._uiId:undefined,
-		equipmentUiId: equipment._uiId
-	}
+export function createDefaultEntry(equipment:EquipmentModel, task: TaskModel | undefined): EntryModel {
+  const uuid = uuidv1();
+  let defaultAge = -1;
+  if (equipment.ageAcquisitionType !== AgeAcquisitionType.time) {
+    defaultAge = equipment.age;
+  }
+
+  return {
+    _uiId: uuid,
+    name: task ? task.name : '',
+    date: new Date(),
+    age: defaultAge,
+    remarks: '',
+    taskUiId: task ? task._uiId : undefined,
+    equipmentUiId: equipment._uiId,
+    ack: true,
+  };
 }
 
 export function updateEntry(entry: EntryModel): EntryModel {
-    entry.date = new Date(entry.date);
-    return entry;
+  entry.date = new Date(entry.date);
+  return entry;
 }
