c3c28c0b4e516eb10069115101d9ef4b34bf95fc
Torruella, Paul <paul.torruella@suez.com>
2020-01-12 12:58:54 +0530

    Test the error feedback for the MyInput component

--- src/components/Form/__tests__/MyInput.test.js
+++ src/components/Form/__tests__/MyInput.test.js
@@ -3,6 +3,7 @@ import { mount } from 'enzyme';
 import ignoredMessages from '../../../testHelpers/MockConsole';
 
 import MyInput from '../MyInput';
+import updateWrapper from '../../../testHelpers/EnzymeHelper';
 
 const defaultMessage = {
   id: 'defaultId',
@@ -27,6 +28,23 @@ describe('MyInput', () => {
     expect(wrapper.find('input').prop('required')).toEqual(true);
     expect(wrapper.find('input').prop('type')).toEqual('text');
     expect(wrapper.find('input').prop('name')).toEqual('name');
+    expect(wrapper.find('FormFeedback').length).toBe(0);
+  });
+
+  it('of type text with an error should render as expected', async () => {
+    // Arrange
+    const wrapper = mount(<MyInput name="name" label={defaultMessage} type="text" required validationTrigger={0} />);
+    const newProps = {
+      name: 'name', label: defaultMessage, type: 'text', required: true, validationTrigger: 1,
+    };
+
+    // Act
+    wrapper.setProps(newProps);
+    await updateWrapper(wrapper);
+
+    // Assert
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('FormFeedback').length).toBe(1);
   });
 
   it('of type checkox should render as expected', () => {
@@ -40,5 +58,22 @@ describe('MyInput', () => {
     expect(wrapper.find('input').prop('required')).toEqual(true);
     expect(wrapper.find('input').prop('type')).toEqual('checkbox');
     expect(wrapper.find('input').prop('name')).toEqual('name');
+    expect(wrapper.find('FormFeedback').length).toBe(0);
+  });
+
+  it('of type checkox with an error should render as expected', async () => {
+    // Arrange
+    const newprops = {
+      name: 'name', label: defaultMessage, type: 'checkbox', required: true, validationTrigger: 1,
+    };
+    const wrapper = mount(<MyInput name="name" label={defaultMessage} type="checkbox" required validationTrigger={0} checked={false} />);
+
+    // Act
+    wrapper.setProps(newprops);
+    await updateWrapper(wrapper);
+
+    // Assert
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('FormFeedback').length).toBe(1);
   });
 });
