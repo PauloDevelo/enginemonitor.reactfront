79eeeee9d18bff811db467175ad04b9cac97c76a
Torruella, Paul <paul.torruella@suez.com>
2020-02-07 17:02:19 +0530

    Add a manager that will manage the current asset.

---
+++ src/services/AssetManager.ts
@@ -0,0 +1,52 @@
+import assetProxy from './AssetProxy';
+
+import userContext from './UserContext';
+
+// eslint-disable-next-line no-unused-vars
+import { AssetModel, UserModel } from '../types/Types';
+
+export interface IAssetManager{
+    getCurrentAsset(): AssetModel | undefined;
+    setCurrentAsset(asset: AssetModel): void;
+
+    registerOnCurrentAssetChanged(listener: (asset: AssetModel) => void):void;
+    unregisterOnCurrentAssetChanged(listenerToRemove: (asset: AssetModel) => void):void;
+}
+
+class AssetManager implements IAssetManager {
+    private listeners: ((asset: AssetModel) => void)[] = [];
+
+    private assets: AssetModel[] = [];
+
+    private currentAsset: AssetModel|undefined = undefined;
+
+    constructor() {
+      userContext.registerOnUserChanged(this.onCurrentUserChanged);
+    }
+
+    // eslint-disable-next-line no-unused-vars
+    onCurrentUserChanged = async (_user: UserModel) => {
+      this.assets = await assetProxy.fetchAssets();
+      this.setCurrentAsset(this.assets.length > 0 ? this.assets[0] : undefined);
+    }
+
+    getCurrentAsset(): AssetModel | undefined {
+      return this.currentAsset;
+    }
+
+    setCurrentAsset(asset: AssetModel) {
+      this.currentAsset = asset;
+      this.listeners.map((listener) => listener(this.currentAsset));
+    }
+
+    registerOnCurrentAssetChanged(listener: (asset: AssetModel) => void):void{
+      this.listeners.push(listener);
+    }
+
+    unregisterOnCurrentAssetChanged(listenerToRemove: (asset: AssetModel) => void):void{
+      this.listeners = this.listeners.filter((listener) => listener !== listenerToRemove);
+    }
+}
+
+const assetManager:IAssetManager = new AssetManager();
+export default assetManager;
