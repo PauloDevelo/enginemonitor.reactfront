e471c3299c23a1f39e2b08a53830158f2ed3b3f5
Torruella, Paul <paul.torruella@suez.com>
2020-02-26 20:19:19 +0200

    Cleanup/refactor StorageUpdateService

--- src/services/StorageUpdaterService.ts
+++ src/services/StorageUpdaterService.ts
@@ -1,6 +1,7 @@
 import * as log from 'loglevel';
 
-const storageVersionKey = 'storageVersion';
+// eslint-disable-next-line no-unused-vars
+import { IStorageService } from './StorageService';
 
 export interface IStorageUpdaterService{
   onUserStorageOpened(): Promise<void>;
@@ -13,48 +14,61 @@ class StorageUpdaterService implements IStorageUpdaterService {
       return;
     }
 
-    const { default: storageService } = await import('./StorageService');
+    try {
+      const { default: storageService } = await import('./StorageService');
+
+      let currentStorageVersion = await storageService.getStorageVersion();
+      log.info(`The storage version is  ${currentStorageVersion}`);
+
+      if (currentStorageVersion < 1) {
+        await this.upgradeStorageToRelease1(storageService);
+      }
+
+      if (currentStorageVersion < 2) {
+        await this.upgradeStorageToRelease2(storageService);
+      }
+
+      currentStorageVersion = await storageService.getStorageVersion();
+      log.info(`The storage is up-to-dated with the version ${currentStorageVersion}`);
+    } catch (error) {
+      log.error(`An error occurred during the upgrade of the storage: ${error.message}`);
+      log.error(error);
+
+      throw error;
+    }
+  }
+
+  private upgradeStorageToRelease1 = async (storageService: IStorageService) => {
+    log.info('Update the storage to the release 1: Add the ack option in the entries');
+
     const { default: equipmentProxy } = await import('./EquipmentProxy');
     const { default: entryProxy } = await import('./EntryProxy');
 
-    let currentStorageVersion = 0;
-    if (await storageService.existItem(storageVersionKey)) {
-      currentStorageVersion = await storageService.getItem<number>(storageVersionKey);
-    }
+    const equipments = await equipmentProxy.getStoredEquipment();
+    const updateEntryPromises = equipments.map(async (equipment) => {
+      try {
+        const entries = await entryProxy.fetchAllEntries({ equipmentId: equipment._uiId, forceToLookUpInStorage: true });
+        const updatedEntries = entries.map((entry) => ({ ack: true, ...entry }));
+        await storageService.setItem(entryProxy.getBaseEntryUrl(equipment._uiId), updatedEntries);
+        log.info(`The entries for the equipment ${equipment._uiId} are updated`);
+      } catch (error) {
+        log.error(error.message);
+      }
+    });
 
-    log.info(`The storage version is  ${currentStorageVersion}`);
+    await Promise.all(updateEntryPromises);
 
-    if (currentStorageVersion < 1) {
-      log.info('Update the storage to the release 1: Add the ack option in the entries');
+    const newCurrentVersion = await storageService.setStorageVersion(1);
+    log.info(`Storage updated to the release ${newCurrentVersion}`);
+  }
 
-      const equipments = await equipmentProxy.getStoredEquipment();
-      const updateEntryPromises = equipments.map(async (equipment) => {
-        try {
-          const entries = await entryProxy.fetchAllEntries({ equipmentId: equipment._uiId, forceToLookUpInStorage: true });
-          const updatedEntries = entries.map((entry) => ({ ack: true, ...entry }));
-          await storageService.setItem(entryProxy.getBaseEntryUrl(equipment._uiId), updatedEntries);
-          log.info(`The entries for the equipment ${equipment._uiId} are updated`);
-        } catch (error) {
-          log.error(error.message);
-        }
-      });
+  private upgradeStorageToRelease2 = async (storageService: IStorageService) => {
+    log.info('Update the storage to the release 2: clear the storage since the url have been changed');
 
-      await Promise.all(updateEntryPromises);
+    await storageService.getUserStorage().clear();
 
-      currentStorageVersion = await storageService.setItem<number>(storageVersionKey, 1);
-      log.info(`Storage updated to the release ${currentStorageVersion}`);
-    }
-
-    if (currentStorageVersion < 2) {
-      log.info('Update the storage to the release 2: clear the storage since the url have been changed');
-
-      await storageService.getUserStorage().clear();
-
-      currentStorageVersion = await storageService.setItem<number>(storageVersionKey, 2);
-      log.info(`Storage updated to the release ${currentStorageVersion}`);
-    }
-
-    log.info(`The storage is up-to-dated with the version ${currentStorageVersion}`);
+    const newStorageVersion = await storageService.setStorageVersion(2);
+    log.info(`Storage updated to the release ${newStorageVersion}`);
   }
 }
 
